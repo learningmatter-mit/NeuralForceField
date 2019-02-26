7192374a2c8a27c034494dd23cae70ada23816f5
wwj <wwj@mit.edu>
2019-02-25 21:04:39 -0500

    dense layer initialization, use adjacency matrix as a filter

--- schnet.py
+++ schnet.py
@@ -9,6 +9,12 @@ import torch.nn.functional as F
 from torch.autograd.gradcheck import zero_gradients
 from torch.autograd import grad
 
+from torch.nn.init import xavier_uniform_
+
+from functools import partial
+from torch.nn.init import constant_
+zeros_initializer = partial(constant_, val=0.)
+
 class GraphDis(torch.nn.Module):
 
     """Compute distance matrix on the fly 
@@ -20,10 +26,9 @@ class GraphDis(torch.nn.Module):
         F (TYPE): Fr + Fe
         Fe (TYPE): edge feature length
         Fr (TYPE): node geature length
-        pbc (TYPE): If True, the distance matrix is computed based on minimum image convetion 
     """
     
-    def __init__(self, Fr, Fe, device, cutoff, pbc=False, box_len=None):
+    def __init__(self, Fr, Fe, device, cutoff, box_len=None):
         super(GraphDis, self).__init__()
 
         self.Fr = Fr
@@ -31,12 +36,10 @@ class GraphDis(torch.nn.Module):
         self.F = Fr + Fe
         self.device = device
         self.cutoff = cutoff
-        self.pbc = pbc
-
-        if pbc == True and box_len==None:
-            raise RuntimeError("require box vector input!")
-        if pbc:
-            self.box_len = torch.Tensor(box_len).cuda(self.device)
+        if box_len is not None:
+            self.box_len = torch.Tensor(box_len).to(self.device)
+        else:
+            self.box_len = None
     
     def get_bond_vector_matrix(self, frame):
         '''
@@ -50,12 +53,12 @@ class GraphDis(torch.nn.Module):
         frame = frame.view(-1, N_atom, 1, 3)
         dis_mat = frame.expand(-1, N_atom, N_atom, 3) - frame.expand(-1, N_atom, N_atom, 3).transpose(1,2)
         
-        if self.pbc:
+        if self.box_len is not None:
 
             # build minimum image convention 
             box_len = self.box_len
-            mask_pos = dis_mat.ge(0.5*box_len).type(torch.FloatTensor).cuda(self.device)
-            mask_neg = dis_mat.lt(-0.5*box_len).type(torch.FloatTensor).cuda(self.device)
+            mask_pos = dis_mat.ge(0.5*box_len).type(torch.FloatTensor).to(self.device)
+            mask_neg = dis_mat.lt(-0.5*box_len).type(torch.FloatTensor).to(self.device)
             
             # modify distance 
             dis_add = mask_neg * box_len
@@ -65,7 +68,7 @@ class GraphDis(torch.nn.Module):
         # create cutoff mask
         dis_sq = dis_mat.pow(2).sum(3)                  # compute squared distance of dim (B, N, N)
         mask = (dis_sq <= cutoff ** 2) & (dis_sq != 0)                 # byte tensor of dim (B, N, N)
-        A = mask.unsqueeze(3).type(torch.FloatTensor).cuda(self.device) #         
+        A = mask.unsqueeze(3).type(torch.FloatTensor).to(self.device) #         
         
         # 1) PBC 2) # gradient of zero distance 
         dis_sq = dis_sq.unsqueeze(3)
@@ -87,9 +90,9 @@ class GraphDis(torch.nn.Module):
 
         # Compute the bond_vector_matrix, which has shape (B, N, N, 3), and append it to the edge matrix
         e, A = self.get_bond_vector_matrix(frame=xyz)# .type(torch).to(device=device.index)
-        e = e.type(torch.FloatTensor).cuda(self.device)
-        A = A.type(torch.FloatTensor).cuda(self.device)
-        #d = d.type(torch.LongTensor).cuda(self.device)
+        e = e.type(torch.FloatTensor).to(self.device)
+        A = A.type(torch.FloatTensor).to(self.device)
+        #d = d.type(torch.LongTensor).to(self.device)
         
         return(r, e, A)
     
@@ -130,15 +133,14 @@ def gaussian_smearing(distances, offset, widths, centered=False):
     if centered == False:
         # Compute width of Gaussians (using an overlap of 1 STDDEV)
         # widths = offset[1] - offset[0]
+        coeff = -0.5 / torch.pow(widths, 2)
         # Use advanced indexing to compute the individual components
-        # diff = distances[:, :, :, None] - offset[None, None, None, :]
-        diff = distances - offset
+        diff = distances[:, :, :, None] - offset[None, None, None, :]
     else:
         # If Gaussians are centered, use offsets to compute widths
         coeff = -0.5 / torch.pow(offset, 2)
         # If centered Gaussians are requested, don't substract anything
-        #diff = distances[:, :, :, None]
-        diff = distances
+        diff = distances[:, :, :, None]
     # Compute and return Gaussians
     gauss = torch.exp(coeff * torch.pow(diff, 2))
     return gauss
@@ -183,27 +185,47 @@ class GaussianSmearing(nn.Module):
         """
         return gaussian_smearing(distances, self.offsets, self.width, centered=self.centered)
 
-class Dense(nn.Module):
-    """ 
-    Applies a dense layer with activation: :math:`y = activation(Wx + b)`
+class Dense(nn.Linear):
+    """ Applies a dense layer with activation: :math:`y = activation(Wx + b)`
 
     Args:
         in_features (int): number of input feature
         out_features (int): number of output features
         bias (bool): If set to False, the layer will not adapt the bias. (default: True)
         activation (callable): activation function (default: None)
+        weight_init (callable): function that takes weight tensor and initializes (default: xavier)
+        bias_init (callable): function that takes bias tensor and initializes (default: zeros initializer)
     """
 
-    def __init__(self, in_features, out_features, bias=True, activation=None):
-        super(Dense, self).__init__()
-        self.linear = torch.nn.Linear(in_features, out_features, bias=bias)
+    def __init__(self, in_features, out_features, bias=True, activation=None,
+                 weight_init=xavier_uniform_, bias_init=zeros_initializer):
+        self.weight_init = weight_init
+        self.bias_init = bias_init
         self.activation = activation
 
+        super(Dense, self).__init__(in_features, out_features, bias)
+
+    def reset_parameters(self):
+        """
+        Reinitialize model parameters.
+        """
+        self.weight_init(self.weight)
+        if self.bias is not None:
+            self.bias_init(self.bias)
+
     def forward(self, inputs):
-        if self.activation is not None:
-            return self.activation(self.linear(inputs))
-        else: 
-            return self.linear(inputs)
+        """
+        Args:
+            inputs (dict of torch.Tensor): SchNetPack format dictionary of input tensors.
+
+        Returns:
+            torch.Tensor: Output of the dense layer.
+        """
+        y = super(Dense, self).forward(inputs)
+        if self.activation:
+            y = self.activation(y)
+
+        return y
 
 def shifted_softplus(x):
     """
@@ -231,34 +253,49 @@ class InteractionBlock(nn.Module):
         
     """
 
-    def __init__(self, n_atom_basis, n_filters, n_gaussians, cutoff_soft):
+    def __init__(self, n_atom_basis, n_filters, n_gaussians, cutoff_soft, trainable_gauss):
         super(InteractionBlock, self).__init__()
-        self.smearing = GaussianSmearing(start=0.0, stop=cutoff_soft, n_gaussians=n_gaussians, trainable=True)
-        self.DistanceFilter1 = Dense(in_features= n_gaussians, out_features=n_gaussians, activation=shifted_softplus)
-        self.DistanceFilter2 = Dense(in_features= n_gaussians, out_features=n_filters)
+        self.smearing = GaussianSmearing(start=0.0, stop=cutoff_soft, n_gaussians=n_gaussians, trainable=trainable_gauss)
+
+        # filter network 
+        self.DistanceFilter1 = Dense(in_features= n_gaussians, out_features=n_filters, activation=shifted_softplus)
+        self.DistanceFilter2 = Dense(in_features= n_filters, out_features=n_filters)
+
+        # in2f
         self.AtomFilter = Dense(in_features=n_atom_basis, out_features=n_filters, bias=False)
+
+        # f2_out
         self.Dense1 = Dense(in_features=n_filters, out_features= n_atom_basis, activation=shifted_softplus)
+
+        #dense
         self.Dense2 = Dense(in_features=n_atom_basis, out_features= n_atom_basis, activation=None)
         
     def forward(self, r, e, A):
-        e = self.smearing(e.squeeze())
+        #e = self.smearing(e.squeeze())
+        e = self.smearing(e.reshape(-1, r.shape[1], r.shape[1]))
         W = self.DistanceFilter1(e)
-        W = self.DistanceFilter2(e)
+        W = self.DistanceFilter2(W)
         
         #r = r[:, None, :, :].expand(-1, r.shape[1], -1, -1) # expand to filtered 
         #print(W.shape)
         r = self.AtomFilter(r)
         # adjacency matrix used as a mask
         A = A.unsqueeze(3).expand(-1, -1, -1, r.shape[2])
-        y = r[:, None, :, :].expand(-1, r.shape[1], -1, -1) * A 
-        y = y * W
-        
-        y = y.sum(2)
+        W = W * A
 
+        # filter 
+        y = r[:, None, :, :].expand(-1, r.shape[1], -1, -1)# * A 
+        y = y * W * A
+        
+        # y = y.sum(2)
+        #y = y * A.sum(2).reciprocal().expand_as(y) # cutoff_network
+        y = y.sum(2)
         y = self.Dense1(y)
+        # y = y * A.sum(2).reciprocal().expand_as(y)
         y = self.Dense2(y)
         
-        #y = y.sum(2) # sum pooling       
+        #y = y.sum(2) # sum pooling 
+        
         return y
 
 class Net(nn.Module):
@@ -273,16 +310,18 @@ class Net(nn.Module):
         
     """
 
-    def __init__(self, n_atom_basis, n_filters, n_gaussians, cutoff_soft, device, T):
+    def __init__(self, n_atom_basis, n_filters, n_gaussians, cutoff_soft, device, T, trainable_gauss, box_len=None):
         super(Net, self).__init__()
         
-        self.graph_dis = GraphDis(Fr=1, Fe=1, cutoff=10.0, device=device)
+        self.graph_dis = GraphDis(Fr=1, Fe=1, cutoff=10.0, box_len = box_len, device=device)
+
 
         self.convolutions = nn.ModuleList([InteractionBlock(n_atom_basis=n_atom_basis,
                                              n_filters=n_filters, n_gaussians=n_gaussians, 
-                                             cutoff_soft =cutoff_soft) for i in range(T)])
+                                             cutoff_soft =cutoff_soft, trainable_gauss=trainable_gauss) for i in range(T)])
 
-        self.atomEmbed = Dense(in_features= 1, out_features=n_atom_basis, bias=False)
+        #self.atomEmbed = Dense(in_features= 1, out_features=n_atom_basis, bias=False)
+        self.atomEmbed = nn.Embedding(100, n_atom_basis, padding_idx=0)
         self.atomwise1 = Dense(in_features= n_atom_basis, out_features= int(n_atom_basis/2), activation=shifted_softplus)
         self.atomwise2 = Dense(in_features= int(n_atom_basis/2), out_features=1)
         
@@ -290,7 +329,9 @@ class Net(nn.Module):
     def forward(self, r, xyz):
         
         r, e ,A = self.graph_dis(r= r, xyz=xyz)
-        r = self.atomEmbed(r)
+
+        #r = self.atomEmbed(r).squeeze()
+        r = self.atomEmbed(r).reshape(r.shape[0], r.shape[1], -1)
 
         for i, conv in enumerate(self.convolutions):
             r = r + conv(r=r, e=e, A=A)
