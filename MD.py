2ec25d0af8a062aa458ce038c3a3c9f789388867
wwj <wwj@mit.edu>
2019-04-15 10:02:24 -0400

    add Fluorine mass

--- MD.py
+++ MD.py
@@ -13,7 +13,7 @@ from ase.md.verlet import VelocityVerlet
 from ase import units
 from ase import Atoms
 
-mass_dict = {6: 12.01, 8: 15.999, 1: 1.008, 3: 6.941, 7: 14.0067}
+mass_dict = {6: 12.01, 8: 15.999, 1: 1.008, 3: 6.941, 7: 14.0067, 9:18.998403}
 ev_to_kcal = 23.06035
 
 
