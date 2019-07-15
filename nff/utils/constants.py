ac5260e4647bce81880078bbc3fecac2bb9a0766
Daniel <dskoda@mit.edu>
2019-07-15 14:53:19 -0400

    useful constants for the project

---
+++ nff/utils/constants.py
@@ -0,0 +1,19 @@
+
+# Energies
+HARTREE_TO_KCAL_MOL = 627.509
+EV_TO_KCAL_MOL = 23.06052
+
+# Distances
+BOHR_RADIUS = 0.529177
+
+# Masses
+ATOMIC_MASS = {
+    1: 1.008,
+    3: 6.941,
+    6: 12.01,
+    7: 14.0067,
+    8: 15.999,
+    9:18.998403,
+    14: 28.0855,
+    16: 32.06,
+}
