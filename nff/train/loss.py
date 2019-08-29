98d51cd6f87c49c9544e43423224b1bf588d3791
Daniel Schwalbe-Koda <dskoda@mit.edu>
2019-08-29 14:35:09 +0200

    nan instead of None

--- nff/train/loss.py
+++ nff/train/loss.py
@@ -38,7 +38,7 @@ def build_mse_loss(loss_coef):
             pred = results[key]
 
             # select only properties which are given
-            valid_idx = (targ != None)
+            valid_idx = torch.isnan(targ)
             targ = targ[valid_idx]
             pred = pred[valid_idx]
 
