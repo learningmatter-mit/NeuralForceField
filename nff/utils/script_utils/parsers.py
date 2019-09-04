ae9dc85dabaa0dbb8abcad1d560077dc23772656
Daniel Schwalbe-Koda <dskoda@mit.edu>
2019-09-04 20:09:13 +0200

    removed loss function arguments

--- nff/utils/script_utils/parsers.py
+++ nff/utils/script_utils/parsers.py
@@ -160,13 +160,6 @@ def add_subparsers(cmd_parser, defaults={}):
         action='store_true',
         help="If set, sets gaussians as learnable parameters (default: False)",
     )
-    schnet_parser.add_argument(
-        "--rho",
-        type=float,
-        default=1.0,
-        help="Parameter multiplying the loss of forces (default: %(default)s)",
-    )
-
 
     ## setup subparser structure
     cmd_subparsers = cmd_parser.add_subparsers(
--- scripts/nff_train.py
+++ scripts/nff_train.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     elif args.mode == "eval":
         # load model
         model = torch.load(os.path.join(args.model_path, "best_model"))
-        loss_fn = build_mse_loss(args.rho)
+        loss_fn = build_mse_loss(args)
         test_loader = get_loaders(args, logging=logging)
 
         # run evaluation
