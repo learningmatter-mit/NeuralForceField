3f077f9b313ebfb2219afebf35b7c2bf76b4796b
Daniel Schwalbe-Koda <dskoda@mit.edu>
2019-09-22 23:20:58 -0400

    removed dependency on model. Assumes the AtomsBatch already has a calculator

--- nff/md/nve.py
+++ nff/md/nve.py
@@ -13,38 +13,34 @@ import nff.utils.constants as const
 from nff.md.utils import NeuralMDLogger, write_traj
 from nff.io.ase import NeuralFF
 
-DEFAULTNVEPARAMS = {'T_init': 300.0, 
-            'time_step': 0.5, 
-            'thermostat': VelocityVerlet, # or Langevin or NPT or NVT or Thermodynamic Integration
-            'nbr_list_update_freq': 50,
-            'steps': 100}
+DEFAULTNVEPARAMS = {
+    'T_init': 300.0, 
+    'time_step': 0.5, 
+    'thermostat': VelocityVerlet,  # or Langevin or NPT or NVT or Thermodynamic Integration
+    'nbr_list_update_freq': 50,
+    'steps': 100
+}
 
-DEFAULTLOGPARAMS = {'save_frequency': 20,
-                    'thermo_filename': './thermo.log', 
-                    'traj_filename': './atom.traj'}
+DEFAULTLOGPARAMS = {
+    'save_frequency': 20,
+    'thermo_filename': './thermo.log', 
+    'traj_filename': './atom.traj'
+}
 
 
 class Dynamics:
     
     def __init__(self, 
                 atomsbatch,
-                model,
                 mdparam=DEFAULTNVEPARAMS,
                 logparam=DEFAULTLOGPARAMS,
                 ):
     
         # initialize the atoms batch system 
-        self.calculator = NeuralFF(model=model, props=atomsbatch.get_batch())
         self.atomsbatch = atomsbatch
         self.mdparam = mdparam
         self.logparam = logparam
    
-        # add calculators 
-        self.atomsbatch.set_calculator(self.calculator)
-        
-        # integration method 
-        integrator = self.mdparam['thermostat']
-        
         # todo: structure optimization before starting
         
         # intialize system momentum 
@@ -86,8 +82,8 @@ class Dynamics:
         
         for snapshot in traj:
             frames = np.concatenate([
-            snapshot.get_atomic_numbers().reshape(-1, 1),
-            snapshot.get_positions().reshape(-1, 3)
+                snapshot.get_atomic_numbers().reshape(-1, 1),
+                snapshot.get_positions().reshape(-1, 3)
             ], axis=1)
             
             xyz.append(frames)
