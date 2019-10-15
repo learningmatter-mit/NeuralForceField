fix offset sign

nff/nn/models.py
@@ -84,7 +84,7 @@ class SchNet(nn.Module):
         xyz.requires_grad = True
 
         # calculating the distances
-        e = (xyz[a[:, 0]] - xyz[a[:, 1]] + offsets).pow(2).sum(1).sqrt()[:, None]
+        e = (xyz[a[:, 0]] - xyz[a[:, 1]] - offsets).pow(2).sum(1).sqrt()[:, None]
 
         # ensuring image atoms have the same vectors of their corresponding
         # atom inside the unit cell