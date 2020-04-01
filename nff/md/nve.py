fb1a9cc09066120fe2c6440d15fe616570213bda
wwj <wwj@mit.edu>
2020-04-01 11:57:31 -0400

    provide restart options

--- nff/md/nve.py
+++ nff/md/nve.py
@@ -53,32 +53,72 @@ class Dynamics:
         
         # attach trajectory dump 
         self.traj = Trajectory(self.mdparam['traj_filename'], 'w', self.atomsbatch)
-        self.integrator.attach(self.traj.write, interval=mdparam['save_frequency'])
+        self.integrator.attach(self.traj.write, interval=self.mdparam['save_frequency'])
         
         # attach log file
         self.integrator.attach(NeuralMDLogger(self.integrator, 
                                         self.atomsbatch, 
                                         self.mdparam['thermo_filename'], 
-                                        mode='a'), interval=mdparam['save_frequency'])
-    
+                                        mode='a'), interval=self.mdparam['save_frequency'])
+        
+    def setup_restart(self, restart_param):
+        """If you want to restart a simulations with predfined mdparams but longer
+         youneed to prodive a dcionary like the following:
+
+         note that the thermo_filename and traj_name should be different 
+
+         restart_param = {'atoms_path': md_log_dir + '/atom.traj', 
+                          'thermo_filename':  md_log_dir + '/thermo_restart.log',
+                          'traj_filename': md_log_dir + '/atom_restart.traj',
+                          'steps': 100
+                          }
+        
+        Args:
+            restart_param (dict): dictionary to contains restart paramsters and file paths
+        """
+
+        if restart_param['thermo_filename'] == self.mdparam['thermo_filename']:
+            raise ValueError("{} is also used, \
+                please change a differnt thermo file name".format(restart_param['thermo_filename']))
+
+        if restart_param['traj_filename'] == self.mdparam['traj_filename']:
+            raise ValueError("{} is also used, \
+                please change a differnt traj file name".format(restart_param['traj_filename']))
+
+        self.restart_param = restart_param
+        new_atoms = Trajectory(restart_param['atoms_path'])[-1]
+        
+        self.atomsbatch.set_positions(new_atoms.get_positions())
+        self.atomsbatch.set_velocities(new_atoms.get_velocities())
+
+        # set thermostats 
+        integrator = self.mdparam['thermostat']
+        self.integrator = integrator(self.atomsbatch, **self.mdparam['thermostat_params'])
+
+        # attach trajectory dump 
+        self.traj = Trajectory(self.restart_param['traj_filename'], 'w', self.atomsbatch)
+        self.integrator.attach(self.traj.write, interval=self.mdparam['save_frequency'])
+
+        # attach log file
+        self.integrator.attach(NeuralMDLogger(self.integrator, 
+                                        self.atomsbatch, 
+                                        self.restart_param['thermo_filename'], 
+                                        mode='a'), interval=self.mdparam['save_frequency'])
+        
+        self.mdparam['steps'] = restart_param['steps']
+        
     def run(self):
-        # 
+         
         epochs = int(self.mdparam['steps'] // self.mdparam['nbr_list_update_freq'])
         
         for step in range(epochs):
             self.integrator.run(self.mdparam['nbr_list_update_freq'])
             self.atomsbatch.update_nbr_list()
-
         self.traj.close()
         
     
     def save_as_xyz(self, filename='./traj.xyz'):
         
-        '''
-        TODO: save time information 
-        TODO: subclass TrajectoryReader/TrajectoryReader to digest AtomsBatch instead of Atoms?
-        TODO: other system variables in .xyz formats 
-        '''
         traj = Trajectory(self.mdparam['traj_filename'], mode='r')
         
         xyz = []
