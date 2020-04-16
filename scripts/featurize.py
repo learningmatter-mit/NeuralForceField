37736b04bf98b144c7117d50542a750fde2e3016
Simon Axelrod <simonaxelrod83@gmail.com>
2020-04-16 17:41:34 -0400

    CHange featurize

--- scripts/featurize.py
+++ scripts/featurize.py
@@ -1,13 +1,15 @@
+from nff.data import Dataset
 import sys
 sys.path.insert(0, "..")
 
-from nff.data import Dataset
 
 def main():
     pth = "dataset.pth.tar"
     dataset = Dataset.from_file(pth)
+    dataset = Dataset(props=dataset.props, units=dataset.units)
     dataset.featurize()
     dataset.save(pth)
+
 
 if __name__ == "__main__":
     main()
