567aad10230e1204af7ef82747f4a921565d93b7
saxelrod <saxelrod@eofe7.cm.cluster>
2020-05-22 15:37:37 -0400

    Remove topology import

--- nff/data/dataset.py
+++ nff/data/dataset.py
@@ -12,7 +12,6 @@ from ase import Atoms
 from ase.neighborlist import neighbor_list
 from torch.utils.data import Dataset as TorchDataset
 from nff.data.sparse import sparsify_tensor
-from nff.data.topology import update_props_topologies
 from nff.data.graphs import reconstruct_atoms, get_neighbor_list, generate_subgraphs, DISTANCETHRESHOLDICT_Z 
 from nff.io.ase import AtomsBatch
 
