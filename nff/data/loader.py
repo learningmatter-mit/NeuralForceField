dbbbc7a7704f7537b527b8cba2197e3f599169c0
Simon Axelrod <simonaxelrod83@gmail.com>
2020-06-12 17:56:07 -0400

    Imbalanced data sampler option

--- nff/data/loader.py
+++ nff/data/loader.py
@@ -53,3 +53,47 @@ def collate_dicts(dicts):
             batch[key] = batch[key].to(dtype)
 
     return batch
+
+class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
+    """
+    Sampling class to make sure positive and negative labels
+    are represented equally during training. 
+    Attributes:
+        data_length (int): length of dataset
+        weights (torch.Tensor): weights of each index in the
+            dataset depending.
+
+    """
+
+    def __init__(self,
+                 target_name,
+                 props):
+        """
+        Args:
+            target_name (str): name of the property being classified
+            props (dict): property dictionary
+        """
+
+        data_length = len(props[target_name])
+
+        negative_idx = [i for i, target in enumerate(
+            props[target_name]) if round(target.item()) == 0]
+        positive_idx = [i for i in range(data_length)
+                        if i not in negative_idx]
+
+        negative_weight = 1 / len(negative_idx)
+        positive_weight = 1 / len(positive_idx)
+
+        self.data_length = data_length
+        self.weights = torch.zeros(data_length)
+        self.weights[negative_idx] = negative_weight
+        self.weights[positive_idx] = positive_weight
+
+    def __iter__(self):
+
+        return (i for i in torch.multinomial(
+            self.weights, self.data_length, replacement=True))
+
+    def __len__(self):
+        return self.data_length
+
