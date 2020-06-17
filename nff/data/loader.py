1169d59f0a99b6bfc83ee47ad7a8b0d1143be3b7
Simon Axelrod <simonaxelrod83@gmail.com>
2020-06-17 16:01:31 -0400

    Distributed sampler wrapper around imbalanced data sampler

--- nff/data/loader.py
+++ nff/data/loader.py
@@ -1,8 +1,15 @@
 import numpy as np
 from collections.abc import Iterable
 import torch
+from torch.utils.data import DistributedSampler
+from torch.utils.data import Dataset, Sampler
+from operator import itemgetter
+
+
+
 import pdb
 from nff.data.topology import ALL_TOPOLOGY_KEYS, RE_INDEX_TOPOLOGY_KEYS
+
 
 REINDEX_KEYS = ['atoms_nbr_list', 'nbr_list', *RE_INDEX_TOPOLOGY_KEYS]
 
@@ -107,3 +114,80 @@ class ImbalancedDatasetSampler(torch.utils.data.sampler.Sampler):
 
     def __len__(self):
         return self.data_length
+
+
+
+class DatasetFromSampler(Dataset):
+    """Dataset of indexes from `Sampler`."""
+
+    def __init__(self, sampler):
+        """
+        Args:
+            sampler (Sampler): @TODO: Docs. Contribution is welcome
+        """
+        self.sampler = sampler
+        self.sampler_list = None
+
+    def __getitem__(self, index):
+        """Gets element of the dataset.
+        Args:
+            index (int): index of the element in the dataset
+        Returns:
+            Single element by index
+        """
+        if self.sampler_list is None:
+            self.sampler_list = list(self.sampler)
+        return self.sampler_list[index]
+
+    def __len__(self):
+        """
+        Returns:
+            int: length of the dataset
+        """
+        return len(self.sampler)
+
+class DistributedSamplerWrapper(DistributedSampler):
+    """
+    Wrapper over `Sampler` for distributed training.
+    Allows you to use any sampler in distributed mode.
+    It is especially useful in conjunction with
+    :class:`torch.nn.parallel.DistributedDataParallel`. In such case, each
+    process can pass a DistributedSamplerWrapper instance as a DataLoader
+    sampler, and load a subset of subsampled data of the original dataset
+    that is exclusive to it.
+    .. note::
+        Sampler is assumed to be of constant size.
+    """
+
+    def __init__(
+        self,
+        sampler,
+        num_replicas=None,
+        rank=None,
+        shuffle=True,
+    ):
+        """
+        Args:
+            sampler: Sampler used for subsampling
+            num_replicas (int, optional): Number of processes participating in
+                distributed training
+            rank (int, optional): Rank of the current process
+                within ``num_replicas``
+            shuffle (bool, optional): If true (default),
+                sampler will shuffle the indices
+        """
+        super(DistributedSamplerWrapper, self).__init__(
+            DatasetFromSampler(sampler),
+            num_replicas=num_replicas,
+            rank=rank,
+            shuffle=shuffle,
+        )
+        self.sampler = sampler
+
+    def __iter__(self):
+        """@TODO: Docs. Contribution is welcome."""
+        self.dataset = DatasetFromSampler(self.sampler)
+        indexes_of_indexes = super().__iter__()
+        subsampler_indexes = self.dataset
+        return iter(itemgetter(*indexes_of_indexes)(subsampler_indexes))
+
