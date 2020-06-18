fdba23847fc191dbe9b53757c06f699b93eb5971
Simon Axelrod <simonaxelrod83@gmail.com>
2020-06-18 16:20:01 -0400

    Get rid of old chemprop models

--- nff/train/builders/model.py
+++ nff/train/builders/model.py
@@ -7,8 +7,6 @@ import torch
 from nff.nn.models.schnet import SchNet, SchNetAuTopology, AuTopology
 from nff.nn.models.hybridgraph import HybridGraphConv
 from nff.nn.models.conformers import WeightedConformers
-from nff.nn.models.chemprop3d import ChemProp3D
-from nff.nn.models.chemprop2d import ChemProp2D
 from nff.nn.models.schnet_features import SchNetFeatures
 
 PARAMS_TYPE = {"SchNet":
@@ -72,25 +70,7 @@ PARAMS_TYPE = {"SchNet":
                    'trainable_gauss': bool,
                    'dropout_rate': float,
                    'readoutdict': dict,
-                   'mol_fp_layers': list                
-                },
-
-                "ChemProp3D":
-                {
-                   'n_atom_basis': int,
-                   'n_filters': int,
-                   'n_gaussians': int,
-                   'n_convolutions': int,
-                   'cutoff': float,
-                   'trainable_gauss': bool,
-                   'dropout_rate': float,
-                   'readoutdict': dict,
                    'mol_fp_layers': list
-                },
-
-                "ChemProp2D":
-                {
-                   'readoutdict': dict,
                 },
 
               "SchNetFeatures":
@@ -113,8 +93,6 @@ MODEL_DICT = {
     "SchNetAuTopology": SchNetAuTopology,
     "HybridGraphConv": HybridGraphConv,
     "WeightedConformers": WeightedConformers,
-    "ChemProp3D": ChemProp3D,
-    "ChemProp2D": ChemProp2D,
     "SchNetFeatures": SchNetFeatures
 }
 
