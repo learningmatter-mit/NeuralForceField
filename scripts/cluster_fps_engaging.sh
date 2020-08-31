aac8e9f381806070cb8960094316e776212f5094
saxelrod <saxelrod@eofe7.cm.cluster>
2020-08-31 17:38:13 -0400

    Fix engaging env

--- scripts/cluster_fps_engaging.sh
+++ scripts/cluster_fps_engaging.sh
@@ -9,6 +9,6 @@ source $HOME/.bashrc
 export PYTHONPATH="/home/saxelrod/Repo/projects/covid_nff/NeuralForceField:${PYTHONPATH}"
 
 source deactivate
-source activate /home/saxelrod/miniconda3/envs/htvs
+source activate /home/saxelrod/anaconda3/envs/htvs
 
 python cluster_fps.py --arg_path cluster_fps_engaging.json
