7b2544d5f032910b2db52fae4aa20e01dfdbe367
Simon Axelrod <simonaxelrod83@gmail.com>
2020-09-25 10:09:57 -0400

    Removed conformer clustering scripts

--- scripts/cluster_fps.sh
+++
@@ -1,7 +0,0 @@
-source $HOME/.bashrc
-export PYTHONPATH="/home/saxelrod/Repo/projects/covid_nff/NeuralForceField:${PYTHONPATH}"
-
-source deactivate
-source activate /home/saxelrod/miniconda3/envs/htvs
-
-python cluster_fps.py --arg_path cluster_fps.json
