65a48bc4dedd698671af33508b3d2cf4ad54095d
Simon Axelrod <saxelrod@mit.edu>
2020-10-01 18:49:06 -0400

    Fix training script

--- scripts/cp3d/train/train_parallel.sh
+++ scripts/cp3d/train/train_parallel.sh
@@ -19,14 +19,14 @@ source activate nff
 export MASTER_ADDR=$(srun -l bash -c 'hostname' | sort | head -1 | awk '{print $2}')
 export MASTER_IP=$(getent hosts `hostname` | cut -d ' ' -f1)
 export MASTER_PORT=8888
+
 # change to the number of GPUs you're using per node
 export SLURM_GPUS_PER_NODE=2
 export LD_LIBRARY_PATH=lib/$CONDA_PREFIX/:$LD_LIBRARY_PATH
-export NFFDIR="$HOME/repo/nff/covid_clean/NeuralForceField"
 
-# this is the problem: something's going wrong in this branch of covid_clean
-# export PYTHONPATH=$NFFDIR:$PYTHON_PATH
-export PYTHONPATH="$PYTHONPATH:$DJANGOCHEMDIR:$HTVSDIR"
+# change to your location of NeuralForceField
+export NFFDIR="$HOME/repo/nff/covid_clean/NeuralForceField"
+export PYTHONPATH=$NFFDIR:$PYTHON_PATH
 
 python $NFFDIR/scripts/cp3d/train/train_parallel.py job_info.json  & pid=$!
 wait
