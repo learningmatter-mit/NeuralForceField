cf5e28be67f9eaacd090738d0589b8322d80999b
Simon Axelrod <simonaxelrod83@gmail.com>
2020-10-03 21:53:29 -0400

    Batch detach for lists

--- nff/utils/cuda.py
+++ nff/utils/cuda.py
@@ -1,7 +1,6 @@
 """Functions to deal with the GPU and the CUDA driver
 """
 
-import torch
 
 def batch_to(batch, device):
     gpu_batch = dict()
@@ -9,19 +8,28 @@ def batch_to(batch, device):
         gpu_batch[key] = val.to(device) if hasattr(val, 'to') else val
     return gpu_batch
 
+
+def detach(val):
+    return val.detach.cpu() if hasattr(val, "detach") else val
+
+
 def batch_detach(batch):
     """detach batch of GPU tensors
-    
+
     Args:
         batch (dict): batches of data/preidction
-    
+
     Returns:
         TYPE: dict
     """
     detach_batch = dict()
     for key, val in batch.items():
-        detach_batch[key] = val.detach().cpu() if hasattr(val, 'detach') else val
+        if type(val) is list:
+            detach_batch[key] = [detach(sub_val) for sub_val in val]
+        else:
+            detach_batch[key] = detach(val)
     return detach_batch
+
 
 def to_cpu(batch):
     cpu_batch = {}
