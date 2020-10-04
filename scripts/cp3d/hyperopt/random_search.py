4bd61a252ce26df3991e189947e9123aa1372921
Simon Axelrod <saxelrod@mit.edu>
2020-10-04 00:16:54 -0400

    Fix hyperopt

--- scripts/cp3d/hyperopt/random_search.py
+++ scripts/cp3d/hyperopt/random_search.py
@@ -21,9 +21,9 @@ def clean_up(model_path):
 
 def run(job_path, model_path, metric):
 
-    cmd = f"cd $NFFDIR/scripts/cp3d/train && bash python $NFFDIR/scripts/cp3d/train/train_parallel.py {job_path}"
+    cmd = f"cd $NFFDIR/scripts/cp3d/train && python $NFFDIR/scripts/cp3d/train/train_parallel.py {job_path}"
     os.system(cmd)
-    best_score, best_epoch = parse_score(job_path, model_path, metric)
+    best_score, best_epoch = parse_score(model_path, metric)
 
     return best_score
 
