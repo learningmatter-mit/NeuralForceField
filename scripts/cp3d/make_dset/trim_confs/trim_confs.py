1253bc37eef63c0c04c3deec20920f60dd8a8ec1
Simon Axelrod <simonaxelrod83@gmail.com>
2020-10-22 18:05:46 -0400

    Docstrings

--- scripts/cp3d/make_dset/trim_confs/trim_confs.py
+++ scripts/cp3d/make_dset/trim_confs/trim_confs.py
@@ -12,7 +12,7 @@ import pdb
 from tqdm import tqdm
 
 from nff.data import Dataset
-from nff.utils import fprint
+from nff.utils import fprint, tqdm_enum
 
 
 def assert_ordered(batch):
@@ -34,21 +34,36 @@ def assert_ordered(batch):
 def get_batch_dic(batch,
                   idx_dic,
                   num_confs):
+    """
+    Get some conformer information about the batch.
+    Args:
+        batch (dict): Properties of the species in this batch
+        idx_dic (dict): Dicationary of the form
+            {smiles: indices}, where `indices` are the indices
+            of the conformers you want to keep. If not specified,
+            then the top `num_confs` conformers with the highest
+            statistical weight will be used.
+        num_confs (int): Number of conformers to keep
+    Returns:
+        info_dic (dict): Dictionary with extra conformer 
+            information about the batch
+
+    """
 
     mol_size = batch["mol_size"]
     old_num_atoms = batch["num_atoms"]
 
-    # if conformers in batch beforehand
-    # are less than num_confs, use
-    # the old number of conformers
-
+    # number of conformers in the batch is number of atoms / atoms per mol
     confs_in_batch = old_num_atoms // mol_size
+
+    # new number of atoms after trimming
     new_num_atoms = int(mol_size * min(
         confs_in_batch, num_confs))
 
     if idx_dic is None:
 
         assert_ordered(batch)
+        # new number of conformers after trimming
         real_num_confs = min(confs_in_batch, num_confs)
         conf_idx = list(range(real_num_confs))
 
@@ -68,15 +83,30 @@ def get_batch_dic(batch,
 
 
 def to_xyz_idx(batch_dic):
+    """
+    Get the indices of the nxyz corresponding to atoms in conformers
+    we want to keep.
+    Args:
+        batch_dic (dict): Dictionary with extra conformer 
+            information about the batch
+    Returns:
+        xyz_conf_all_idx (torch.LongTensor): nxyz indices of atoms
+            in the conformers we want to keep.
+    """
 
     confs_in_batch = batch_dic["confs_in_batch"]
     mol_size = batch_dic["mol_size"]
+    # Indices of the conformers we're keeping (usually of the form [0, 1, ...
+    # `max_confs`], unless specified otherwise)
     conf_idx = batch_dic["conf_idx"]
 
     # the xyz indices of where each conformer starts
     xyz_conf_start_idx = [i * mol_size for i in range(confs_in_batch + 1)]
     # a list of the full set of indices for each conformer
     xyz_conf_all_idx = []
+
+    # go through each conformer index, get the corresponding xyz indices,
+    # and append them to xyz_conf_all_idx
 
     for conf_num in conf_idx:
 
@@ -86,6 +116,7 @@ def to_xyz_idx(batch_dic):
 
         xyz_conf_all_idx.append(full_idx)
 
+    # concatenate into tensor
     xyz_conf_all_idx = torch.cat(xyz_conf_all_idx)
 
     return xyz_conf_all_idx
@@ -95,19 +126,36 @@ def split_nbrs(nbrs,
                mol_size,
                confs_in_batch,
                conf_idx):
+    """
+    Get the indices of the neighbor list that correspond to conformers 
+    we're keeping.
+    Args:
+        nbrs (torch.LongTensor): neighbor list
+        mol_size (int): Number of atoms in each conformer
+        confs_in_batch (int): Total number of conformers in the batch
+        conf_idx (list[int]): Indices of the conformers we're keeping 
+    Returns:
+        tens_idx (torch.LongTensor): nbr indices of conformers we're
+            keeping.
+    """
 
     split_idx = []
+    # The cutoff of the atom indices for each conformer
     cutoffs = [i * mol_size - 1 for i in range(1, confs_in_batch + 1)]
 
     for i in conf_idx:
 
-        start = cutoffs[i] - mol_size
+        # start index of the conformer
+        start = cutoffs[i] - mol_size + 1
+        # end index of the conformer
         end = cutoffs[i]
+
+        # mask so that all elements of the neighbor list are
+        # >= start and <= end
         mask = (nbrs[:, 0] <= end) * (nbrs[:, 1] <= end)
         mask *= (nbrs[:, 0] >= start) * (nbrs[:, 1] >= start)
 
         idx = mask.nonzero().reshape(-1)
-
         split_idx.append(idx)
 
     tens_idx = torch.cat(split_idx)
@@ -116,6 +164,16 @@ def split_nbrs(nbrs,
 
 
 def to_nbr_idx(batch_dic, nbrs):
+    """
+    Apply `split_nbrs` given `batch_dic`
+    Args:
+        batch_dic (dict): Dictionary with extra conformer 
+            information about the batch
+        nbrs (torch.LongTensor): neighbor list
+    Returns:
+        split_nbr_idx (torch.LongTensor): nbr indices of conformers we're
+            keeping. 
+    """
 
     mol_size = batch_dic["mol_size"]
     confs_in_batch = batch_dic["confs_in_batch"]
@@ -130,6 +188,15 @@ def to_nbr_idx(batch_dic, nbrs):
 
 
 def update_weights(batch, batch_dic):
+    """
+    Readjust weights so they sum to 1.
+    Args:
+        batch_dic (dict): Dictionary with extra conformer 
+            information about the batch
+        batch (dict): Batch dictionary
+    Returns:
+        new_weights (torch.Tensor): renormalized weights
+    """
 
     old_weights = batch["weights"]
 
@@ -143,6 +210,20 @@ def update_weights(batch, batch_dic):
 
 
 def convert_nbrs(batch_dic, nbrs, nbr_idx):
+    """
+    Convert neighbor list to only include neighbors from the conformers
+    we're looking at.
+    Args:
+        batch_dic (dict): Dictionary with extra conformer 
+            information about the batch
+        nbrs (torch.LongTensor): neighbor list
+        nbr_idx (torch.LongTensor): nbr indices of conformers we're
+            keeping. 
+    Returns:
+        new_nbrs (torch.LongTensor): updated neighbor list
+
+    """
+
     conf_idx = batch_dic["conf_idx"]
     mol_size = batch_dic["mol_size"]
     new_nbrs = []
@@ -158,6 +239,18 @@ def convert_nbrs(batch_dic, nbrs, nbr_idx):
 
 
 def update_dset(batch, batch_dic, dataset, i):
+    """
+    Update the dataset with the new values for the requested
+    number of conformers, for species at index i.
+    Args:
+        batch (dict): Batch dictionary
+        batch_dic (dict): Dictionary with extra conformer 
+            information about the batch
+        dataset (nff.data.dataset): NFF dataset
+        i (int): index of the species whose info we're updating
+    Returns:
+        dataset (nff.data.dataset): updated NFF dataset
+    """
 
     bond_nbrs = batch["bonded_nbr_list"]
     nbr_list = batch["nbr_list"]
@@ -165,13 +258,20 @@ def update_dset(batch, batch_dic, dataset, i):
     atom_feats = batch["atom_features"]
     nxyz = batch["nxyz"]
 
+    # get the indices of the nyxz, nbr list, and bonded
+    # nbr list that are contained within the requested number
+    # of conformers
+
     conf_xyz_idx = to_xyz_idx(batch_dic)
     bond_nbr_idx = to_nbr_idx(batch_dic, bond_nbrs)
     all_nbr_idx = to_nbr_idx(batch_dic, nbr_list)
 
+    # change the number of atoms to the proper value
     dataset.props["num_atoms"][i] = batch_dic["new_num_atoms"]
+    # get the right nxyz
     dataset.props["nxyz"][i] = nxyz[conf_xyz_idx]
 
+    # convert the neighbor lists
     dataset.props["bonded_nbr_list"][i] = convert_nbrs(batch_dic,
                                                        bond_nbrs,
                                                        bond_nbr_idx)
@@ -180,22 +280,31 @@ def update_dset(batch, batch_dic, dataset, i):
                                                 nbr_list,
                                                 all_nbr_idx)
 
+    # get the atom and bond features at the right indices
     dataset.props["bond_features"][i] = bond_feats[bond_nbr_idx]
     dataset.props["atom_features"][i] = atom_feats[conf_xyz_idx]
+
+    # renormalize weights
     dataset.props["weights"][i] = update_weights(batch,
                                                  batch_dic)
 
     return dataset
 
 
-def tqdm_enum(iter):
-    i = 0
-    for y in tqdm(iter):
-        yield i, y
-        i += 1
-
-
 def trim_confs(dataset, num_confs, idx_dic):
+    """
+    Trim conformers for the entire dataset.
+    Args:
+        dataset (nff.data.dataset): NFF dataset
+        num_confs (int): desired number of conformers
+        idx_dic (dict): Dicationary of the form
+            {smiles: indices}, where `indices` are the indices
+            of the conformers you want to keep. If not specified,
+            then the top `num_confs` conformers with the highest
+            statistical weight will be used.
+    Returns:
+        dataset (nff.data.dataset): updated NFF dataset
+    """
 
     for i, batch in tqdm_enum(dataset):
 
@@ -216,6 +325,21 @@ def main(from_model_path,
          num_confs,
          conf_file,
          **kwargs):
+    """
+    Load the dataset, reduce the number of conformers, and save it.
+    Args:
+        from_model_path (str): The path to the folder in which
+            the old dataset is saved.
+        to_model_path (str): The path to the folder in which
+            the new dataset will be saved.
+        num_confs (int): Desired number of conformers per species
+        conf_file (str): Path to the JSON file that tells you which
+            conformer indices to use for each species.
+    Returns:
+        None
+    """
+
+    # load `conf_file` if given
 
     if conf_file is not None:
         with open(conf_file, "r") as f:
@@ -223,13 +347,17 @@ def main(from_model_path,
     else:
         idx_dic = None
 
+    # If the folder has sub_folders 0, 1, ..., etc.,
+    # then load each dataset in each sub-folder. Otherwise
+    # the dataset must be in the main folder.
+
     folders = sorted([i for i in os.listdir(from_model_path)
                       if i.isdigit()], key=lambda x: int(x))
 
-    # then the dataset is in the main folder
-
     if folders == []:
         folders = [""]
+
+    # Go through each dataset, update it, and save it
 
     for folder in tqdm(folders):
 
