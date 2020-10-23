1ce64e7b016605e4e2cd0c537eb39c42ce1b7234
Simon Axelrod <simonaxelrod83@gmail.com>
2020-10-22 21:05:08 -0400

    Docstrings

--- scripts/cp3d/hyperopt/random_search.py
+++ scripts/cp3d/hyperopt/random_search.py
@@ -8,6 +8,13 @@ from nff.utils import fprint, parse_args, parse_score, METRICS
 
 
 def clean_up(model_path):
+    """
+    Clean up any files leftover from past runs with different hyperparameters.
+    Args:
+      model_path (str): directory of model and dataset
+    Returns:
+      None
+    """
     cmds = ["rm */*.pickle",
             "rm -rf check*",
             "rm */train_len",
@@ -21,8 +28,18 @@ def clean_up(model_path):
 
 
 def run(job_path, model_path, metric):
+    """
+    Train a model with the current hyperparameters.
+    Args:
+      job_path (str): path to the folder with the job config file
+      model_path (str): directory of model and dataset
+      metric (str): metric by which to evaluate model performance
+    Returns:
+      best_score (float): best validation score from the model
+    """
 
-    cmd = f"cd $NFFDIR/scripts/cp3d/train && python train_parallel.py {job_path}"
+    cmd = (f"cd $NFFDIR/scripts/cp3d/train "
+           f"&& python train_parallel.py {job_path}")
     os.system(cmd)
     best_score, best_epoch = parse_score(model_path, metric)
 
@@ -33,6 +50,16 @@ def update_dropout(info,
                    dropout,
                    dropout_type,
                    prop_name):
+    """
+    Update the config information with new dropout values.
+    Args:
+      info (dict): job information in the config file
+      dropout (float): value of the new dropout
+      dropout_type (str): type of dropout (i.e. in SchNet,
+        ChemProp, readout or attention)
+    Returns:
+      None
+    """
 
     if dropout_type == "schnet_dropout":
         info["model_params"]["schnet_dropout"] = dropout
@@ -41,6 +68,8 @@ def update_dropout(info,
         info["model_params"]["cp_dropout"] = dropout
 
     elif dropout_type == "readout_dropout":
+        # if it's in the readout layers, find the dropout
+        # layers in the readout dictionary and update them
         readout = info["model_params"]["readoutdict"]
         layer_dics = readout[prop_name]
         for layer_dic in layer_dics:
@@ -57,8 +86,17 @@ def update_dropout(info,
 
 def update_heads(info,
                  heads):
+    """
+    Update the config information with the number of attention heads.
+    Args:
+      info (dict): job information in the config file
+      heads (int): number of attention heads
+    Returns:
+      None
+    """
 
     info["model_params"]["boltzmann_dict"]["num_heads"] = heads
+    # Concatenate the fingerprints produced by the different heads
     info["model_params"]["boltzmann_dict"]["head_pool"] = "concatenate"
 
     readoutdict = info["model_params"]["readoutdict"]
@@ -68,12 +106,24 @@ def update_heads(info,
     for key, lst in readoutdict.items():
         for i, dic in enumerate(lst):
             if "param" in dic and "in_features" in dic.get("param", {}):
+                # make sure that the input dimension to the readout is equal to
+                # `heads * feat_dim`, where `feat_dim` is the feature dimension
+                # produced by each head
                 readoutdict[key][i]["param"]["in_features"] = feat_dim * heads
                 break
     info["model_params"]["readoutdict"] = readoutdict
 
 
 def update_general(info, key, val):
+    """
+    Update a general parameter that's in the main info dictionary.
+    Args:
+      info (dict): job information in the config file
+      key (str): name of the parameter
+      val (Union[float, str, int]): its value
+    Returns:
+      None
+    """
 
     info["model_params"][key] = val
 
@@ -82,6 +132,16 @@ def update_info(job_path,
                 vals,
                 param_names,
                 prop_name):
+    """
+    Update the config information and save it.
+    Args:
+      job_path (str): path to the folder with the job config file
+      vals (list): new values to use
+      param_names (list[str]): names of the parameters being updated
+      prop_name (str): Name of property you're predicting
+    Returns:
+      None
+    """
 
     with open(job_path, "r") as f:
         info = json.load(f)
@@ -91,12 +151,15 @@ def update_info(job_path,
 
     for param_name, val in zip(param_names, vals):
         if param_name.startswith("log_"):
+            # if anything starts with "log_" (e.g. "log_schnet_dropout"),
+            # exponentiate its value to get the actual number
             real_names.append(param_name.replace("log_", ""))
             real_vals.append(np.exp(val))
         else:
             real_names.append(param_name)
             real_vals.append(val)
 
+    # update values
     for param_type, val in zip(real_names, real_vals):
         if 'dropout' in param_type:
             update_dropout(info=info,
@@ -122,11 +185,21 @@ def update_info(job_path,
 
             update_general(info, key=param_type, val=val)
 
+    # save
     with open(job_path, "w") as f:
         json.dump(info, f, indent=4, sort_keys=True)
 
 
 def sample_vals(options, param_types):
+    """
+    Do a random sample of values.
+    Args:
+      options (list): a set of options for each parameter
+      param_types (list[str]): what kind of value each parameter
+        is (categorical, int or float)
+    Returns:
+      vals (list): sampled values
+    """
 
     vals = []
 
@@ -134,8 +207,11 @@ def sample_vals(options, param_types):
 
         param_type = param_types[i]
 
+        # if categorical, sample one of the options randomly
         if param_type == "categorical":
             val = random.choice(lst)
+
+        # otherwise sample between the minimum and maximum values
 
         elif param_type in ["int", "float"]:
 
@@ -162,22 +238,43 @@ def main(job_path,
          prop_name,
          param_types,
          **kwargs):
+    """
+    Sample hyperparmeters and save scores for each combination.
+    Args:
+      job_path (str): path to the folder with the job config file
+      model_path (str): directory of model and dataset
+      options (list): a set of options for each parameter
+      num_samples (int): how many combinations of hyperparams to try
+      metric (str): metric by which to evaluate model performance
+      score_file (str): name of file in which you'll store scores
+      param_names (list[str]): names of the parameters being updated
+      prop_name (str): Name of property you're predicting
+      param_types (list[str]): what kind of value each parameter
+        is (categorical, int or float)
+    Returns:
+      None
+    """
 
     dic_path = os.path.join(model_path, score_file)
     score_list = []
 
+    # load old scores if they exist
     if os.path.isfile(dic_path):
         with open(dic_path, "r") as f:
             score_list = json.load(f)
 
+    # iterate over hyperparameter combinations
     for _ in range(num_samples):
 
+        # clean up model folder from previous interation
         clean_up(model_path=model_path)
+        # sample hyperparam values
         vals = sample_vals(options, param_types)
         val_str = "  " + "\n  ".join([f"{key}: {val}" for key, val
-                  in zip(param_names, vals)])
+                                      in zip(param_names, vals)])
         fprint(f"Hyperpameters used this round:\n{val_str}")
 
+        # update config file, run, get the score, and save
         update_info(job_path=job_path,
                     vals=vals,
                     param_names=param_names,
