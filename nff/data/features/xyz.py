4b6691666710d4c24e258dcd2c80bb3ec6a60d35
Simon Axelrod <saxelrod@.mit.edu>
2020-10-26 23:47:18 -0400

    Docstrings

--- nff/data/features/xyz.py
+++ nff/data/features/xyz.py
@@ -1,17 +1,44 @@
+"""
+Tools for generating xyz-baed features
+"""
+
 from rdkit import Chem
 from rdkit.Chem import rdMolDescriptors as rdMD
 import torch
 
 
 def get_conformer(xyz):
+    """
+    Create an RDKit conformer object from an xyz.
+    Args:
+        xyz (torch.Tensor): atom type and xyz of geometry.
+    Returns:
+        conformer (rdkit.Chem.Conformer): RDKit conformer
+    """
     n_atoms = len(xyz)
+    # initialize a conformer of the right length
     conformer = Chem.Conformer(n_atoms)
+    # set each atom's positions in the order of the xyz
     for i, nxyz in zip(range(n_atoms), xyz):
         conformer.SetAtomPosition(i, nxyz[1:])
     return conformer
 
 
 def get_mol(xyz, smiles, with_conformer=True):
+    """
+    Get an RDKit mol from an xyz and smiles. Note that this
+    assumes that the xyz is ordered in the same way an RDKit
+    object of the same smiles would be ordered, and that there
+    is no change in connectivity between the RDKit mol and the
+    xyz.
+
+    Args:
+        xyz (torch.Tensor): atom type and xyz of geometry.
+        smiles (str): SMILES string
+        with_conformer (bool): also add conformer to the RDKit mol
+    Returns:
+        mol (rdkit.Chem.rdchem.Mol): RDKit mol object
+    """
 
     mol = Chem.MolFromSmiles(smiles)
     mol = Chem.AddHs(mol)
@@ -23,6 +50,15 @@ def get_mol(xyz, smiles, with_conformer=True):
 
 
 def get_3d_representation(xyz, smiles, method, mol=None):
+    """
+    Args:
+        xyz (torch.Tensor): atom type and xyz of geometry.
+        smiles (str): SMILES string
+        method (str): RDKit method for 3D representation
+        mol (rdkit.Chem.rdchem.Mol): RDKit mol object
+    Returns:
+        result (np.array): fingerprint 
+    """
 
     representation_fn = {
         'autocorrelation_3d': rdMD.CalcAUTOCORR3D,
@@ -32,23 +68,37 @@ def get_3d_representation(xyz, smiles, method, mol=None):
         'getaway': lambda x: rdMD.CalcWHIM(x, precision=0.001)
     }
 
+    # if a `mol` is not given, generate it from the xyz and smiles
     if mol is None:
         mol = get_mol(xyz=xyz, smiles=smiles)
     fn = representation_fn[method]
+    result = fn(mol)
 
-    return fn(mol)
+    return result
 
 
 def featurize_rdkit(dataset, method):
+    """
+    Featurize a dataset with RDKit 3D descriptors.
+    Args:
+        dataset (nff.data.dataset): NFF dataset
+        method (str): RDKit method for 3D representation
+    Returns:
+        None
+    """
     dataset.props[method] = []
     props = dataset.props
 
+    # go through each geometry
     for i in range(len(props['nxyz'])):
 
         smiles = props['smiles'][i]
         nxyz = props['nxyz'][i]
 
         reps = []
+
+        # if there are RDKit mols in the dataset, you can
+        # get the 3D representation from the mol itself
 
         if 'rd_mols' in props:
             rd_mols = props['rd_mols'][i]
@@ -58,8 +108,16 @@ def featurize_rdkit(dataset, method):
                                                          method=method,
                                                          mol=rd_mol))
                 reps.append(rep)
+
+        # otherwise you can get the mols from the nxyz, but this
+        # assumes the same ordering of nxyz and RDKit mol generated
+        # from smiles, which triggers a warning
+
         else:
             nxyz_list = [nxyz]
+
+            # if `mol_size` is there then split the nxyz into conformer
+            # geomtries
             if 'mol_size' in props:
                 mol_size = props['mol_size'][i].item()
                 n_confs = nxyz.shape[0] // mol_size
