09acdc0050fa40fccac769ab55d71145694987da
Simon Axelrod <saxelrod@.mit.edu>
2020-11-04 12:36:49 -0500

    More organized files for different chemprop trains

--- scripts/cp3d/chemprop/cp_train.sh
+++ scripts/cp3d/chemprop/cp_train.sh
@@ -2,5 +2,5 @@ source deactivate
 source ~/.bashrc
 source activate nff
 
-python cp_train.py --config_file "cp_train_config.json"
+python cp_train.py --config_file "files/free_energy/cp_train_config.json"
 
