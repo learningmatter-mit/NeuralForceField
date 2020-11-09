5089dd9c01a2943bdaada4444b7fcb85514b4d1e
Simon Axelrod <saxelrod@.mit.edu>
2020-11-08 20:03:49 -0500

    More config refactoring

--- scripts/cp3d/transfer/run_cp/predict.sh
+++ scripts/cp3d/transfer/run_cp/predict.sh
@@ -3,7 +3,7 @@ source ~/.bashrc
 source activate nff
 
 # change to your config files
-CONFIG="config/predict_config.json"
+CONFIG="config/schnet_feat_cov1/predict_config.json"
 
 cmd="python predict.py --config_file $CONFIG"
 echo $cmd
