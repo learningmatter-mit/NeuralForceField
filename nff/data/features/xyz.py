958d31071e8a6a55db5c8fcbb21ea27d3bcde00a
Simon Axelrod <saxelrod@.mit.edu>
2020-11-13 16:40:00 -0500

    Moved e3fp fingerprinting from features.graph to fratures.xyz, made analysis module for analyzing fingerprints

--- nff/data/features/xyz.py
+++ nff/data/features/xyz.py
@@ -2,9 +2,14 @@
 Tools for generating xyz-baed features
 """
 
+import logging
+
 from rdkit import Chem
 from rdkit.Chem import rdMolDescriptors as rdMD
 import torch
+from e3fp.pipeline import fprints_from_mol
+
+from tqdm import tqdm
 
 
 def get_conformer(xyz):
@@ -57,7 +62,7 @@ def get_3d_representation(xyz, smiles, method, mol=None):
         method (str): RDKit method for 3D representation
         mol (rdkit.Chem.rdchem.Mol): RDKit mol object
     Returns:
-        result (np.array): fingerprint 
+        result (np.array): fingerprint
     """
 
     representation_fn = {
@@ -139,3 +144,62 @@ def featurize_rdkit(dataset, method):
 
         reps = torch.stack(reps)
         dataset.props[method].append(reps)
+
+
+def get_e3fp(mol, bits, smiles=None):
+    """
+    Get an E3FP fingerprint from an RDKit mol.
+    Args:
+        mol (rdkit.Chem.rdchem.Mol): RDKit mol object
+        bits (int): Number of bits in fingerprint
+    Returns:
+        fp (np.array): fingerprint as numpy array
+    """
+    if smiles is None:
+        smiles = Chem.MolToSmiles(mol)
+    mol.SetProp("_Name", smiles)
+    fprint_params = {"bits": bits}
+    fp = (fprints_from_mol(mol, fprint_params=fprint_params)[0]
+          .to_vector().toarray().astype(int)
+          ).reshape(-1)
+    return fp
+
+
+def add_e3fp(rd_dataset,
+             fp_length,
+             verbose=False,
+             track=True):
+    """
+    Add E3FP fingerprints to each conformer in the dataset.
+    Args:
+        rd_dataset (nff.data.dataset): NFF dataset with RDKit mols.
+        fp_length (int): length of each fingerprint
+        verbose (bool): whether to print the progress made
+        track (bool): whether to track progress with tqdm_enum
+    Returns:
+        rd_dataset (nff.data.dataset): NFF dataset updated with E3FP
+    """
+
+    # disable verbose logging from e3fp
+
+    if not verbose:
+        logger = logging.getLogger()
+        logger.disabled = True
+
+    e3fp_list = []
+    func = tqdm if (track) else lambda x: x
+
+    for batch in func(rd_dataset):
+        mols = batch["rd_mols"]
+        smiles = batch["smiles"]
+        fps = []
+        for mol in mols:
+
+            fp_array = get_e3fp(mol, fp_length, smiles)
+            fps.append(torch.Tensor(fp_array))
+
+        e3fp_list.append(torch.stack(fps))
+
+    rd_dataset.props['e3fp'] = e3fp_list
+
+    return rd_dataset
