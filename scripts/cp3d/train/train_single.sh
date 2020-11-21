3cbfd8c0738936aa203730a54bed3fd6064c7699
Simon Axelrod <saxelrod@.mit.edu>
2020-11-20 19:57:28 -0500

    Deal with weird edge case in fps

--- scripts/cp3d/train/train_single.sh
+++ scripts/cp3d/train/train_single.sh
@@ -3,16 +3,15 @@ source ~/.bashrc
 source activate nff
 
 # change to your config path
-CONFIG="config/schnet_feat_cov1.json"
+CONFIG="config/cp3d_cov2_cl_single.json"
 
 # change to your location of NeuralForceField
-export NFFDIR="$HOME/repo/nff/covid_clean/NeuralForceField"
+export NFFDIR="$HOME/Repo/projects/master/NeuralForceField"
 export PYTHONPATH=$NFFDIR:$PYTHON_PATH
 
-cmd="python train_single.py $CONFIG -nr 0 --gpus 1 --nodes 1 & pid=\$!"
+cmd="python train_single.py $CONFIG -nr 0 --gpus 1 --nodes 1 "
 echo $cmd
 eval $cmd
-wait
 
 
 
