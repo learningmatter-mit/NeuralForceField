cd8a9e0dd627085556a6d8f57ba7dd523093b43b
Simon Axelrod <saxelrod@.mit.edu>
2021-02-13 14:48:11 -0500

    Cuda OOM wrapper around evaluate

--- nff/train/evaluate.py
+++ nff/train/evaluate.py
@@ -1,5 +1,10 @@
+import copy
+import torch
+from tqdm import tqdm
+
 from nff.utils.cuda import batch_to, batch_detach
 from nff.data.dataset import concatenate_dict
+
 
 def evaluate(model,
              loader,
@@ -21,22 +26,35 @@ def evaluate(model,
     all_results = []
     all_batches = []
 
-    for batch in loader:
-        # append batch_size
-        batch = batch_to(batch, device)
-
+    for batch in tqdm(loader):
         vsize = batch['nxyz'].size(0)
         n_eval += vsize
 
-        # e.g. if the result is a sum of results from two models, and you just
-        # want the prediction of one of those models
-        if submodel is not None:
-            results = getattr(model, submodel)(batch)
-        else:
-            results = model(batch, **kwargs)
+        use_device = copy.deepcopy(device)
+        while True:
+            batch = batch_to(batch, use_device)
+            model.to(use_device)
+            try:
+                # e.g. if the result is a sum of results from two models,
+                # and you just want the prediction of one of those models
+                if submodel is not None:
+                    results = getattr(model, submodel)(batch)
+                else:
+                    results = model(batch, **kwargs)
+                results = batch_to(batch_detach(results), use_device)
+                eval_batch_loss = loss_fn(batch, results).data.cpu().numpy()
 
-        results = batch_to(batch_detach(results), device)
-        eval_batch_loss = loss_fn(batch, results).data.cpu().numpy()
+                break
+
+            except RuntimeError as err:
+                if 'CUDA out of memory' in str(err):
+                    print(("CUDA out of memory. Doing this batch "
+                           "on cpu."))
+                    use_device = "cpu"
+                    torch.cuda.empty_cache()
+
+                else:
+                    raise err
 
         if loss_is_normalized:
             eval_loss += eval_batch_loss * vsize
@@ -46,8 +64,8 @@ def evaluate(model,
         all_results.append(batch_detach(results))
         all_batches.append(batch_detach(batch))
 
-        # del results
-        # del batch
+        del results
+        del batch
 
     # weighted average over batches
     if loss_is_normalized:
