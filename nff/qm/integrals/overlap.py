51313a60e9efd2ce09e61e27fdfa0ba70fa283de
Simon Axelrod <saxelrod@.mit.edu>
2021-03-22 19:55:31 -0400

    Overlaps to torch

--- nff/qm/integrals/overlap.py
+++ nff/qm/integrals/overlap.py
@@ -1,43 +1,46 @@
-import numba as nb
+# import numba as nb
 import numpy as np
 
+import torch
 
 # @nb.jit(nopython=True)
-def numba_horizontal(s, p, r_pa):
+
+
+def horizontal(s, p, r_pa):
 
     num = s.shape[0]
 
     for i in range(1, num):
-        s[i, :] = r_pa * s[i - 1, :]
+        s[i, :] = r_pa * s[i - 1, :].clone()
 
         if i > 1:
             extra = 1 / (2 * p) * ((i - 1) * s[i - 2, :])
-            s[i, :] += extra
+            s[i, :] = s[i, :] + extra
 
     return s
 
 
 # @nb.jit(nopython=True)
-def numba_vertical(s, r_pb, p):
+def vertical(s, r_pb, p):
 
     l_1 = s.shape[2]
 
-    i_range = np.arange(s.shape[1])
+    i_range = torch.arange(s.shape[1])
     r_pb_shape = r_pb.reshape(-1, 1)
 
-    zeros = np.zeros((3, 1))
+    zeros = torch.zeros(3, 1)
 
     for j in range(1, l_1):
 
-        s_term = r_pb_shape * s[:, :, j - 1]
-        new_s = np.concatenate((zeros, s[:, :-1, j - 1]), axis=1)
+        s_term = r_pb_shape * s[:, :, j - 1].clone()
+        new_s = torch.cat((zeros, s[:, :-1, j - 1]), dim=1)
         i_term = i_range / (2 * p) * new_s
 
         s[:, :, j] = (s_term + i_term)
 
         if j > 1:
             j_term = (j - 1) / (2 * p) * s[:, :, j - 2]
-            s[:, :, j] += j_term
+            s[:, :, j] = s[:, :, j] + j_term
 
     return s
 
@@ -57,7 +60,7 @@ def get_prelims(r_a,
     r_pa = big_p - r_a
     r_pb = big_p - r_b
 
-    s_0 = np.sqrt(np.pi / p) * np.exp(-mu * r_ab ** 2)
+    s_0 = np.sqrt(np.pi / p) * torch.exp(-mu * r_ab ** 2)
 
     return r_pa, r_pb, s_0, p
 
@@ -69,16 +72,16 @@ def compute_overlaps(l_0,
                      r_pb,
                      s_0):
 
-    s = np.zeros((l_0, 3))
+    s = torch.zeros(l_0, 3)
     s[0, :] = s_0
 
-    s = numba_horizontal(s, p, r_pa)
-    s_t = (s.transpose().reshape(3, 1, -1)
-           .transpose(0, 2, 1))
+    s = horizontal(s, p, r_pa)
+    s_t = (s.transpose(0, 1).reshape(3, 1, -1)
+           .transpose(1, 2))
 
-    zeros = np.zeros((3, l_0, l_1 - 1))
-    s = np.concatenate([s_t, zeros], axis=2)
-    s = numba_vertical(s, r_pb, p)
+    zeros = torch.zeros((3, l_0, l_1 - 1))
+    s = torch.cat([s_t, zeros], dim=2)
+    s = vertical(s, r_pb, p)
 
     return s
 
@@ -106,8 +109,12 @@ def pos_to_overlaps(r_a,
 
 
 def test():
-    r_a = np.array([1, 2, 3])
-    r_b = np.array([1.1, 1.8, 2.3])
+    r_a = torch.Tensor([1, 2, 3])
+    r_b = torch.Tensor([1.1, 1.8, 2.3])
+
+    r_a.requires_grad = True
+    r_b.requires_grad = True
+
     a = 1
     b = 2
     l_0 = 4
@@ -119,6 +126,10 @@ def test():
                         b,
                         l_0,
                         l_1)
+
+    loss = s.sum()
+    loss.backward()
+
     idx_pairs = [[0, 1, 1], [0, 1, 2], [1, 2, 1]]
 
     # from numerical integration in Mathematica
@@ -131,4 +142,5 @@ def test():
 
 
 if __name__ == "__main__":
-    test()
+    with torch.autograd.set_detect_anomaly(True):
+        test()
