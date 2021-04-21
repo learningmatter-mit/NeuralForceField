d255d60b005f7a6c938f5e8d250fc1540d962870
Minkai Xu <mkxu@foxmail.com>
2021-04-21 11:36:34 +0000

    new fpai (equivariant but not energy-conserving)

--- scripts/forceconv/run_fpai.py
+++ scripts/forceconv/run_fpai.py
@@ -42,14 +42,14 @@ if params['id'] == None:
         name=logdir,
         metrics=[dict(name='loss', objective='minimize')],
         parameters=[
-            # dict(name='feat_dim', type="int", bounds=dict(min=6, max=9), default_value=7),  # 64->512
-            # dict(name='num_conv', type='int', bounds=dict(min=2, max=4), default_value=3),
-            dict(name='n_rbf', type='int', bounds=dict(min=2, max=6), default_value=3),  # 4->32
-            dict(name='batch_size', type='int', bounds=dict(min=2, max=6), default_value=3),  # 4->32
-            dict(name='cutoff', type='double', bounds=dict(min=4.0, max=8.0), default_value=7),
+            dict(name='num_conv', type='int', bounds=dict(min=2, max=4), default_value=3),
+            dict(name='feat_dim', type="int", bounds=dict(min=6, max=8), default_value=7),  # 64->256
+            dict(name='n_rbf', type='int', bounds=dict(min=2, max=6), default_value=3),  # 4->64
+            dict(name='batch_size', type='int', bounds=dict(min=2, max=5), default_value=3),  # 4->32
+            dict(name='cutoff', type='double', bounds=dict(min=4.0, max=8.0), default_value=7, precision=1),
             dict(name='lr', type='double', bounds=dict(min=1e-4, max=1e-3), default_value=5e-4, transformation="log"),
             # dict(name='conv_dropout', type='double', bounds=dict(min=0.0, max=0.1), default_value=0.0, precision=1),
-            dict(name='readout_dropout', type='double', bounds=dict(min=0.0, max=0.5), default_value=0.0),
+            # dict(name='readout_dropout', type='double', bounds=dict(min=0.0, max=0.5), default_value=0.0, precision=1),
         ],
         observation_budget=n_obs, # how many iterations to run for the optimization
         parallel_bandwidth=10,
@@ -66,13 +66,12 @@ while experiment.progress.observation_count < experiment.observation_budget:
     # tranform parameter s
     trainparam = suggestion.assignments
     ## exp process
-    # trainparam['feat_dim'] = 2**trainparam['feat_dim']
+    trainparam['feat_dim'] = 2**trainparam['feat_dim']
     trainparam['n_rbf'] = 2**trainparam['n_rbf']
     trainparam['batch_size'] = 2**trainparam['batch_size']
     ## constant
     trainparam['conv_dropout'] = 0.0
-    trainparam['num_conv'] = 3
-    trainparam['feat_dim'] = 256
+    trainparam['readout_dropout'] = 0.0
     print(trainparam)
 
     test_mae = train(params, trainparam, suggestion.id, ForcePai, params['epoch'])
