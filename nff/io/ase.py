More fixes to rmsd gradient in neural metadynamics - should be working

nff/io/ase.py
@@ -732,9 +732,9 @@ class NeuralMetadynamics(NeuralFF):
                            atoms,
                            p_grads):
 
-        ref_nxyz_lst = dsets[1].props['nxyz']
+        ref_nxyz_lst = dsets[0].props['nxyz']
         num_refs = len(ref_nxyz_lst)
-        current_nxyz_lst = dsets[0].props['nxyz']
+        current_nxyz_lst = dsets[1].props['nxyz']
         num_atoms = current_nxyz_lst[0].shape[0]
 
         delta_i = delta_i.reshape(-1, 1, 1)
@@ -744,17 +744,24 @@ class NeuralMetadynamics(NeuralFF):
         current_xyz = (torch.stack(current_nxyz_lst * num_refs)
                        [:, :, 1:])
 
-        rmsd_grad = torch.zeros(ref_xyz.shape[0], len(atoms), 3)
+        num_atoms = len(atoms)
+        rmsd_grad = torch.zeros(ref_xyz.shape[0], num_atoms, 3)
         keep_idx = self.get_keep_idx(atoms)
 
         # is this right? This assumes we say that we go from (current_xyz - past)
         # to (R.current_xyz - past), and so we only need grad(R.current_xyz). I think
         # this is the way it's set up based on how we choose dset_0 and dset_1 in
         # `compute_distances`, but need to check
-        
-        rmsd_grad[:, keep_idx] = torch.stack(p_grads)
 
-        v_grad = (v_bias * (-2 * alpha_i * delta_i) * rmsd_grad).sum(0)
+        rot_current = torch.einsum('kij,klj->kli', R_mat, current_xyz)
+        delta_atomwise = (rot_current - ref_xyz)
+
+        rmsd_grad[:, keep_idx] = ((1 / (delta_i * num_atoms)) *
+                                  delta_atomwise * torch.stack(p_grads))
+
+        v_grads = (v_bias * (-2 * alpha_i * delta_i) * rmsd_grad)
+        v_grad = v_grads.sum(0)
+
         force = -v_grad
 
         return force
@@ -764,19 +771,10 @@ class NeuralMetadynamics(NeuralFF):
             return np.zeros((len(atoms), 3))
 
         k_i, alpha_i, dsets, f_damp = self.rmsd_prelims(atoms)
-        try:
-            import pdb
-            pdb.set_trace()
-
-            delta_i, R_mat, p_grads = compute_distances(dataset=dsets[0],
-                                                        device=self.device,
-                                                        dataset_1=dsets[1],
-                                                        get_p_grad=True)
-
-        except Execption as e:
-            print(e)
-            import pdb
-            pdb.post_mortem()
+        delta_i, R_mat, p_grads = compute_distances(dataset=dsets[0],
+                                                    device=self.device,
+                                                    dataset_1=dsets[1],
+                                                    get_p_grad=True)
 
         # compute bias potential
         v_bias = (f_damp * k_i *
@@ -833,6 +831,7 @@ class NeuralMetadynamics(NeuralFF):
 
         f_bias = self.get_bias(atoms)
         self.results['forces'] += f_bias
+        self.results['f_bias'] = f_bias
 
         if add_steps:
             for i, step in enumerate(self.steps_from_old):

nff/utils/geom.py
@@ -114,7 +114,7 @@ def minimize_rotation_and_translation(targ_nxyz,
     p0 = targ_nxyz[:, :, 1:]
 
     c = p.mean(1).reshape(-1, 1, 3)
-    p -= c
+    p = p - c
 
     c0 = p0.mean(1).reshape(-1, 1, 3)
     p0 -= c0
@@ -128,13 +128,19 @@ def minimize_rotation_and_translation(targ_nxyz,
     new_p = torch.einsum("ijk,ilk->ijl", p_repeat, R)
 
     if get_p_grad:
-        p_grad = compute_grad(inputs=p,
-                              output=new_p).detach()
+
+        p_grads = []
+        for this_new in new_p:
+
+            p_grad = compute_grad(inputs=p,
+                                  output=this_new).detach()
+            p_grads.append(p_grad.reshape(-1, 3))
+
         p = p.detach()
     else:
-        p_grad = None
+        p_grads = None
 
-    return new_p, p0, R, p_grad
+    return new_p, p0, R, p_grads
 
 
 def compute_rmsd(targ_nxyz,
@@ -183,9 +189,9 @@ def compute_distance(targ_nxyz,
     R = R.cpu()
 
     if get_p_grad:
-        return distances, R
-    else:
         return distances, R, p_grad
+    else:
+        return distances, R
 
 
 def compute_distances(dataset,
@@ -233,10 +239,9 @@ def compute_distances(dataset,
                                    atom_nxyz=atom_nxyz,
                                    get_p_grad=get_p_grad)
             if get_p_grad:
-                distances, R, p_grad = out
+                distances, R, this_p_grad = out
                 num_atoms = batch_1["num_atoms"].tolist()
-                split_grads = list(torch.split(p_grad, num_atoms))
-                p_grads += split_grads
+                p_grads += this_p_grad
             else:
                 distances, R = out
 
@@ -250,10 +255,10 @@ def compute_distances(dataset,
             all_indices[:, 1] += j_start
 
             distance_mat[all_indices[:, 0],
-                         all_indices[:, 1]] = distances.reshape(-1)
+                         all_indices[:, 1]] = distances.detach().reshape(-1)
 
             R_mat[all_indices[:, 0],
-                  all_indices[:, 1]] = R
+                  all_indices[:, 1]] = R.detach()
 
             j_start += num_mols_1
 