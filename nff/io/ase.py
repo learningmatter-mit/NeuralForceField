d6bed75263c6e33b79f75660148d41428599a603
Simon Axelrod <saxelrod@.mit.edu>
2022-01-03 23:58:41 -0500

    Clean up mTD

--- nff/io/ase.py
+++ nff/io/ase.py
@@ -738,7 +738,7 @@ class NeuralMetadynamics(NeuralFF):
         v_bias = (f_damp * k_i *
                   torch.exp(-alpha_i * delta_i.reshape(-1) ** 2)).sum()
 
-        v_grad = compute_grad(inputs=p,
+        v_grad = compute_grad(inputs=xyz_list[0],
                               output=v_bias).sum(0)
 
         f_bias = -v_grad
