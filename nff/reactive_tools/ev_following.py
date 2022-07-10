0211a8a16ec357346f16b93f786f9b7fe8bfa6ce
Simon Axelrod <saxelrod@.mit.edu>
2022-07-10 19:02:52 -0400

    Undo eig changes

--- nff/reactive_tools/ev_following.py
+++ nff/reactive_tools/ev_following.py
@@ -53,13 +53,11 @@ def eigvec_following(ev_atoms,
         hessian = powell_update(hessian_old, h_old, gradient_old, grad)
 
     # eigenvectors are stored in a transposed form
-    eigenvalues, eigvecs = torch.linalg.eig(hessian)
-    eigenvalues = torch.real(eigenvalues)
-    eigvecs = torch.real(eigvecs)
+    eigenvalues, eigvecs = torch.eig(hessian, eigenvectors=True)
 
     # Ordering eigenvalues and eigenvectors in ascending order
-    idx = eigenvalues.argsort()
-    eigenvalues = eigenvalues[idx]
+    idx = eigenvalues[:, 0].argsort()
+    eigenvalues = eigenvalues[idx][:, 0]
     # print(eigenvalues)
 
     eigvecs = eigvecs[:, idx]
@@ -69,7 +67,7 @@ def eigvec_following(ev_atoms,
     F = torch.mv(eigvecs_t, grad).reshape(-1, 1)
 
     matrix_p = torch.Tensor([[eigenvalues[0], F[0]], [F[0], 0]])
-    lambda_p = torch.linalg.eigvalsh(matrix_p)[1]
+    lambda_p = torch.symeig(matrix_p, eigenvectors=False)[0][1]
 
     matrix_n = torch.zeros(Ndim * len(old_xyz[0]),
                            Ndim * len(old_xyz[0]))
@@ -79,7 +77,7 @@ def eigvec_following(ev_atoms,
         matrix_n[i][Ndim * len(old_xyz[0]) - 1] = F[i + 1]
         matrix_n[Ndim * len(old_xyz[0]) - 1][i] = F[i + 1]
 
-    lambda_n = torch.linalg.eigvalsh(matrix_n)[0]
+    lambda_n = torch.symeig(matrix_n, eigenvectors=False)[0][0]
 
     lambda_n = lambda_n.new_full(
         (Ndim * len(old_xyz[0]) - 1,), lambda_n.item()).to(device)
