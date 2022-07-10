bc9e1e3277b9bda7cdcfb468ee349fed66fa3127
Simon Axelrod <saxelrod@.mit.edu>
2022-07-10 18:33:02 -0400

    Fix eig deprecation

--- nff/reactive_tools/ev_following.py
+++ nff/reactive_tools/ev_following.py
@@ -53,7 +53,7 @@ def eigvec_following(ev_atoms,
         hessian = powell_update(hessian_old, h_old, gradient_old, grad)
 
     # eigenvectors are stored in a transposed form
-    eigenvalues, eigvecs = torch.linalg.eig(hessian, eigenvectors=True)
+    eigenvalues, eigvecs = torch.linalg.eig(hessian)
 
     # Ordering eigenvalues and eigenvectors in ascending order
     idx = eigenvalues[:, 0].argsort()
