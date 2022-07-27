d1f4f7efe21d44c54fc25ff64f715bd974ddc010
Simon Axelrod <saxelrod@.mit.edu>
2022-07-27 17:10:40 -0400

    Fix sum_and_grad issue for DimeNet

--- nff/nn/modules/schnet.py
+++ nff/nn/modules/schnet.py
@@ -49,8 +49,8 @@ def get_rij(xyz,
     # to catch atoms that become neighbors between nbr
     # list updates)
     dist = (r_ij.detach() ** 2).sum(-1) ** 0.5
- 
-    if type(cutoff)==torch.Tensor:
+
+    if type(cutoff) == torch.Tensor:
         dist = dist.to(cutoff.device)
     use_nbrs = (dist <= cutoff)
 
@@ -1131,11 +1131,22 @@ def sum_and_grad(batch,
 
         mol_idx = torch.arange(len(N)).repeat_interleave(
             torch.LongTensor(N)).to(val.device)
+        dim_size = mol_idx.max() + 1
 
-        pooled_result = scatter_add(val.reshape(-1),
+        if val.reshape(-1).shape[0] == mol_idx.shape[0]:
+            use_val = val.reshape(-1)
+
+        # summed atom features
+        elif val.shape[0] == mol_idx.shape[0]:
+            use_val = val.sum(-1)
+
+        else:
+            raise Exception(("Don't know how to handle val shape "
+                             "{} for key {}" .format(val.shape, key)))
+
+        pooled_result = scatter_add(use_val,
                                     mol_idx,
-                                    dim_size=mol_idx.max() + 1)
-
+                                    dim_size=dim_size)
         if mean:
             pooled_result = pooled_result / torch.Tensor(N).to(val.device)
 
