b61206f88871bce0fd9118d965eb947d262744fa
jdiet <jdiet@mit.edu>
2024-01-04 09:12:31 -0500

    New calculator to sample new configs by uncertainty

--- nff/io/bias_calculators.py
+++ nff/io/bias_calculators.py
@@ -26,7 +26,7 @@ class BiasBase(NeuralFF):
 
     Args:
         model: the deural force field model
-        cv_def: lsit of Collective Variable (CV) definitions
+        cv_def: list of Collective Variable (CV) definitions
             [["cv_type", [atom_indices], np.array([minimum, maximum]), bin_width], [possible second dimension]]
         equil_temp: float temperature of the simulation (important for extended system dynamics)
     """
@@ -1023,6 +1023,344 @@ class WTMeABF(eABF):
 
         return bias_force.reshape(-1, 1), local_pot
 
+    
+class AttractiveBias(NeuralFF):
+    """Biased Calculator that introduces an attractive term
+       Designed to be used with UQ as CV
+
+    Args:
+        model: the deural force field model
+        cv_def: list of Collective Variable (CV) definitions
+            [["cv_type", [atom_indices], np.array([minimum, maximum]), bin_width], [possible second dimension]]
+        gamma: coupling strength, regulates strength of attraction
+    """
+
+    implemented_properties = [
+        "energy",
+        "forces",
+        "stress",
+        "energy_unbiased",
+        "forces_unbiased",
+        "cv_vals",
+        "ext_pos",
+        "cv_invmass",
+        "grad_length",
+        "cv_grad_lengths",
+        "cv_dot_PES",
+        "const_vals",
+    ]
+
+    def __init__(
+        self,
+        model,
+        cv_defs: list[dict],
+        gamma=1.0,
+        device="cpu",
+        en_key="energy",
+        directed=DEFAULT_DIRECTED,
+        extra_constraints: list[dict] = None,
+        **kwargs,
+    ):
+
+        NeuralFF.__init__(
+            self, model=model, device=device, en_key=en_key, directed=directed, **kwargs
+        )
+
+        self.gamma = gamma
+        self.cv_defs = cv_defs
+        self.num_cv = len(cv_defs)
+        self.the_cv = []
+        for cv_def in self.cv_defs:
+            self.the_cv.append(CV(cv_def["definition"]))
+
+        self.ext_coords = np.zeros(shape=(self.num_cv, 1))
+        self.ranges = np.zeros(shape=(self.num_cv, 2))
+        self.margins = np.zeros(shape=(self.num_cv, 1))
+        self.conf_k = np.zeros(shape=(self.num_cv, 1))
+
+        for ii, cv in enumerate(self.cv_defs):
+            if "range" in cv.keys():
+                self.ext_coords[ii] = cv["range"][0]
+                self.ranges[ii] = cv["range"]
+            else:
+                raise KeyError("range")
+
+            if "margin" in cv.keys():
+                self.margins[ii] = cv["margin"]
+
+            if "conf_k" in cv.keys():
+                self.conf_k[ii] = cv["conf_k"]
+
+            if "type" not in cv.keys():
+                self.cv_defs[ii]["type"] = "not_angle"
+            else:
+                self.cv_defs[ii]["type"] = cv["type"]
+
+        self.constraints = None
+        self.num_const = 0
+        if extra_constraints is not None:
+            self.constraints = []
+            for cv in extra_constraints:
+                self.constraints.append({})
+
+                self.constraints[-1]["func"] = CV(cv["definition"])
+
+                self.constraints[-1]["pos"] = cv["pos"]
+                if "k" in cv.keys():
+                    self.constraints[-1]["k"] = cv["k"]
+                elif "sigma" in cv.keys():
+                    self.constraints[-1]["k"] = (units.kB * self.equil_temp) / (
+                        cv["sigma"] * cv["sigma"]
+                    )
+                else:
+                    raise KeyError("k/sigma")
+
+                if "type" not in cv.keys():
+                    self.constraints[-1]["type"] = "not_angle"
+                else:
+                    self.constraints[-1]["type"] = cv["type"]
+
+            self.num_const = len(self.constraints)
+
+    def diff(
+        self, a: Union[np.ndarray, float], b: Union[np.ndarray, float], cv_type: str
+    ) -> Union[np.ndarray, float]:
+        """get difference of elements of numbers or arrays
+        in range(-inf, inf) if is_angle is False or in range(-pi, pi) if is_angle is True
+        Args:
+            a: number or array
+            b: number or array
+        Returns:
+            diff: element-wise difference (a-b)
+        """
+        diff = a - b
+
+        # wrap to range(-pi,pi) for angle
+        if isinstance(diff, np.ndarray) and cv_type == "angle":
+
+            diff[diff > np.pi] -= 2 * np.pi
+            diff[diff < -np.pi] += 2 * np.pi
+
+        elif cv_type == "angle":
+
+            if diff < -np.pi:
+                diff += 2 * np.pi
+            elif diff > np.pi:
+                diff -= 2 * np.pi
+
+        return diff
+
+    def step_bias(
+        self,
+        xi: np.ndarray,
+        grad_xi: np.ndarray,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """energy and gradient of bias
+
+        Args:
+            curr_cv: current value of the cv
+            cv_index: for multidimensional FES
+
+        Returns:
+            bias_ener: bias energy
+            bias_grad: gradiant of the bias in CV space, needs to be dotted with the cv_gradient
+        """
+
+        bias_grad = - (self.gamma * grad_xi).sum(axis=0)
+        bias_ener = - (self.gamma * xi).sum()
+
+        # harmonic walls for confinement to range of interest
+        for i in range(self.num_cv):
+            if xi[i] > (self.ranges[i][1] + self.margins[i]):
+                r = self.diff(
+                    self.ranges[i][1] + self.margins[i],
+                    xi[i],
+                    self.cv_defs[i]["type"],
+                )
+                bias_grad += self.conf_k[i] * r
+                bias_ener += 0.5 * self.conf_k[i] * r**2
+
+            elif xi[i] < (self.ranges[i][0] - self.margins[i]):
+                r = self.diff(
+                    self.ranges[i][0] - self.margins[i],
+                    xi[i],
+                    self.cv_defs[i]["type"],
+                )
+                bias_grad += self.conf_k[i] * r
+                bias_ener += 0.5 * self.conf_k[i] * r**2
+
+        return bias_ener, bias_grad
+
+    def harmonic_constraint(
+        self,
+        xi: np.ndarray,
+        grad_xi: np.ndarray,
+    ) -> Tuple[np.ndarray, np.ndarray]:
+        """energy and gradient of additional harmonic constraint
+
+        Args:
+            xi: current value of constraint "CV"
+            grad_xi: Cartesian gradient of these CVs
+
+        Returns:
+            constr_ener: constraint energy
+            constr_grad: gradient of the constraint energy
+
+        """
+
+        constr_grad = np.zeros_like(grad_xi[0])
+        constr_ener = 0.0
+
+        for i in range(self.num_const):
+            dxi = self.diff(
+                xi[i], self.constraints[i]["pos"], self.constraints[i]["type"]
+            )
+            constr_grad += self.constraints[i]["k"] * dxi * grad_xi[i]
+            constr_ener += 0.5 * self.constraints[i]["k"] * dxi**2
+
+        return constr_ener, constr_grad
+
+    def calculate(
+        self,
+        atoms=None,
+        properties=[
+            "energy",
+            "forces",
+            "energy_unbiased",
+            "forces_unbiased",
+            "cv_vals",
+            "cv_invmass",
+            "grad_length",
+            "cv_grad_lengths",
+            "cv_dot_PES",
+            "const_vals",
+        ],
+        system_changes=all_changes,
+    ):
+        """Calculates the desired properties for the given AtomsBatch.
+
+        Args:
+        atoms (AtomsBatch): custom Atoms subclass that contains implementation
+            of neighbor lists, batching and so on. Avoids the use of the Dataset
+            to calculate using the models created.
+        properties: list of keywords that can be present in self.results
+        system_changes (default from ase)
+        """
+
+        if not any([isinstance(self.model, i) for i in UNDIRECTED]):
+            check_directed(self.model, atoms)
+
+        # for backwards compatability
+        if getattr(self, "properties", None) is None:
+            self.properties = properties
+
+        Calculator.calculate(self, atoms, self.properties, system_changes)
+
+        # run model
+        batch = batch_to(atoms.get_batch(), self.device)
+
+        # add keys so that the readout function can calculate these properties
+        grad_key = self.en_key + "_grad"
+        batch[self.en_key] = []
+        batch[grad_key] = []
+
+        kwargs = {}
+        requires_stress = "stress" in self.properties
+        if requires_stress:
+            kwargs["requires_stress"] = True
+        if getattr(self, "model_kwargs", None) is not None:
+            kwargs.update(self.model_kwargs)
+
+        prediction = self.model(batch, **kwargs)
+
+        # change energy and force to numpy array and eV
+        model_energy = prediction[self.en_key].detach().cpu().numpy() * (
+            1 / const.EV_TO_KCAL_MOL
+        )
+
+        if grad_key in prediction:
+            model_grad = prediction[grad_key].detach().cpu().numpy() * (
+                1 / const.EV_TO_KCAL_MOL
+            )
+        else:
+            raise KeyError(grad_key)
+
+        inv_masses = 1.0 / atoms.get_masses()
+        M_inv = np.diag(np.repeat(inv_masses, 3).flatten())
+
+        cvs = np.zeros(shape=(self.num_cv, 1))
+        cv_grads = np.zeros(
+            shape=(
+                self.num_cv,
+                atoms.get_positions().shape[0],
+                atoms.get_positions().shape[1],
+            )
+        )
+        cv_grad_lens = np.zeros(shape=(self.num_cv, 1))
+        cv_invmass = np.zeros(shape=(self.num_cv, 1))
+        cv_dot_PES = np.zeros(shape=(self.num_cv, 1))
+        for ii, cv_def in enumerate(self.cv_defs):
+            xi, xi_grad      = self.the_cv[ii](atoms)
+            cvs[ii]          = xi
+            cv_grads[ii]     = xi_grad
+            cv_grad_lens[ii] = np.linalg.norm(xi_grad)
+            cv_invmass[ii]   = np.einsum('i,ii,i', xi_grad.flatten(), M_inv, xi_grad.flatten())
+            cv_dot_PES[ii]   = np.dot(xi_grad.flatten(), model_grad.flatten())
+            
+        self.results = {
+            "energy_unbiased": model_energy.reshape(-1),
+            "forces_unbiased": -model_grad.reshape(-1, 3),
+            "grad_length": np.linalg.norm(model_grad),
+            "cv_vals": cvs,
+            "cv_grad_lengths": cv_grad_lens,
+            "cv_invmass": cv_invmass,
+            "cv_dot_PES": cv_dot_PES,
+        }
+
+        bias_ener, bias_grad = self.step_bias(cvs, cv_grads)
+        energy = model_energy + bias_ener
+        grad = model_grad + bias_grad
+
+        if self.constraints:
+            consts = np.zeros(shape=(self.num_const, 1))
+            const_grads = np.zeros(
+                shape=(
+                    self.num_const,
+                    atoms.get_positions().shape[0],
+                    atoms.get_positions().shape[1],
+                )
+            )
+            for ii, const_dict in enumerate(self.constraints):
+                consts[ii], const_grads[ii] = const_dict["func"](atoms)
+
+            const_ener, const_grad = self.harmonic_constraint(consts, const_grads)
+            energy += const_ener
+            grad += const_grad
+
+        self.results.update(
+            {
+                "energy": energy.reshape(-1),
+                "forces": -grad.reshape(-1, 3),
+                "ext_pos": self.ext_coords,
+            }
+        )
+
+        if self.constraints:
+            self.results["const_vals"] = consts
+
+        if requires_stress:
+            stress = prediction["stress_volume"].detach().cpu().numpy() * (
+                1 / const.EV_TO_KCAL_MOL
+            )
+            self.results["stress"] = stress * (1 / atoms.get_volume())   
+    
+    
+    
+    
+    
+    
+    
+    
 
 def welford_var(
     count: float, mean: float, M2: float, newValue: float
