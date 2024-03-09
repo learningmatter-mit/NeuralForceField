c5fd20293c204ca8ec7dde962450d2943d39ec48
alex <ajhoff29@mit.edu>
2024-03-09 17:29:25 -0500

    MAINT: update to MACE wrapper
    - Remove old NFF wrapper that did not inherit from pre-trained model
    - Add function to download the MACE pre-trained model if it's not
    already available
    - Update wrapper parameters and forward function for wrapper to work
    with training
    - add a `load` function that works similarly for that in the CHGNet
    wrapper that is more flexible and easier to use than the `from_file`
    function

--- nff/nn/models/mace.py
+++ nff/nn/models/mace.py
@@ -5,159 +5,105 @@
 # See LICENSE for more info
 ####################################################################################################
 
+import os
+import urllib
 from mace.modules.models import MACE, ScaleShiftMACE
 from mace.data.utils import Configuration
 from mace.data.atomic_data import AtomicData, AtomicNumberTable, torch_geometric
-from mace.tools.torch_geometric.batch import Batch
 from mace.tools import torch_tools
 from mace.calculators.mace import get_model_dtype
 from typing import Union
 import torch
-import torch.nn as nn
-from nff.io.ase import AtomsBatch
 
-INIT_PROPS = [
-    "r_max",
-    "num_bessel",
-    "num_polynomial_cutoff",
-    "max_ell",
-    "interaction_cls",
-    "interaction_cls_first",
-    "num_interactions",
-    "num_elements",
-    "hidden_irreps",
-    "MLP_irreps",
-    "atomic_energies",
-    "avg_num_neighbors",
-    "atomic_numbers",
-    "correlation",
-    "gate",
-]
+# get the path to NFF models dir, which is the parent directory of this file
+module_dir = os.path.abspath(
+    os.path.join(os.path.abspath(__file__), "..", "..", "..", "models")
+)
+print(module_dir)
+LOCAL_MODEL_PATH = os.path.join(
+    module_dir, "foundations_models/2023-12-03-mace-mp.model"
+)
+
+MACE_URLS = dict(
+    small="https://tinyurl.com/2jmmb8b7",  # 2023-12-10-mace-128-L0_energy_epoch-249.model
+    medium="https://tinyurl.com/y7uhwpje",  # 2023-12-03-mace-128-L1_epoch-199.model
+    large="https://figshare.com/ndownloader/files/43117273",
+)
 
 
-class NFFMACEWrapper(nn.Module):
-    """Wrapper for the MACE model that allows for interfacing with NFF
-    models. This wrapper converts AtomsBatch objects to the format
-    required by the MACE model and adds energy gradients to the output
-    from a forward pass that computes the forces."""
+def get_mace_mp_model(model: str = None) -> str:
+    """Get the default MACE MP model. Replicated from the MACE codebase,
+    Copyright (c) 2022 ACEsuit/mace and licensed under the MIT license.
 
-    def __init__(
-        self, mace_model: Union[MACE, ScaleShiftMACE], default_dtype="", **kwargs
-    ):
-        """Initializes the NFFMACEWrapper
+    Args:
+        model (str, optional): MACE_MP model that you want to get.
+            Defaults to None. Can be "small", "medium", "large", or a URL.
 
-        Args:
-            mace_model (Union[MACE, ScaleShiftMACE]): a MACE model, can be loaded
-                from a file of a pre-trained model
-            default_dtype (str, optional): datatype for the torch model. Defaults
-                to "", which will match whatever is in the model file. Can also
-                be "float32" or "float64".
-        """
-        super().__init__()
-        self.mace_model = mace_model
-        self.z_table = AtomicNumberTable(
-            [int(z) for z in self.mace_model.atomic_numbers]
+    Raises:
+        RuntimeError: raised if the model download fails and no local model is found
+
+    Returns:
+        str: path to the model
+    """
+    if model in (None, "medium") and os.path.isfile(LOCAL_MODEL_PATH):
+        model = LOCAL_MODEL_PATH
+        print(
+            f"Using local medium Materials Project MACE model for MACECalculator {model}"
         )
-        self.r_max = self.mace_model.r_max.item()
-        model_dtype = get_model_dtype(self.mace_model)
-        if default_dtype == "":
-            print(
-                f"No dtype selected, switching to {model_dtype} to match model dtype."
+    elif model in (None, "small", "medium", "large") or str(model).startswith("https:"):
+        try:
+            checkpoint_url = (
+                MACE_URLS.get(model, MACE_URLS["medium"])
+                if model in (None, "small", "medium", "large")
+                else model
             )
-            default_dtype = model_dtype
-        if model_dtype != default_dtype:
-            print(
-                f"Default dtype {default_dtype} does not match model dtype {model_dtype}, converting models to {default_dtype}."
+            cache_dir = os.path.expanduser("~/.cache/mace")
+            checkpoint_url_name = "".join(
+                c for c in os.path.basename(checkpoint_url) if c.isalnum() or c in "_"
             )
-            if default_dtype == "float64":
-                self.mace_model = self.mace_model.double()
-            elif default_dtype == "float32":
-                self.mace_model = self.mace_model.float()
-        torch_tools.set_default_dtype(default_dtype)
+            cached_model_path = f"{cache_dir}/{checkpoint_url_name}"
+            if not os.path.isfile(cached_model_path):
+                os.makedirs(cache_dir, exist_ok=True)
+                # download and save to disk
+                print(f"Downloading MACE model from {checkpoint_url!r}")
+                urllib.request.urlretrieve(checkpoint_url, cached_model_path)
+                print(f"Cached MACE model to {cached_model_path}")
+            model = cached_model_path
+            msg = f"Loading Materials Project MACE with {model}"
+            print(msg)
+        except Exception as exc:
+            raise RuntimeError(
+                "Model download failed and no local model found"
+            ) from exc
 
-    def forward(self, batch, **kwargs):
-        data = self.convert_atomsbatch_to_data(batch)
-        output = self.mace_model.forward(data)
-        output = self.add_engrad_to_output(output)
-        return output
-
-    def convert_atomsbatch_to_data(self, batch):
-        if not isinstance(batch, dict):
-            props = batch.get_batch()
-        else:
-            props = batch
-        cum_idx_list = [0] + torch.cumsum(props["num_atoms"], 0).tolist()
-        dataset = []
-
-        for i in range(props.get("num_atoms").shape[0]):
-            node_idx = torch.arange(cum_idx_list[i], cum_idx_list[i + 1])
-            positions = props.get("nxyz")[node_idx, 1:].cpu().numpy()
-            numbers = props.get("nxyz")[node_idx, 0].long().cpu().numpy()
-
-            if "cell" in props.keys():
-                cell = props["cell"][3 * i : 3 * i + 3].cpu().numpy()
-            elif "lattice" in props.keys():
-                cell = props["lattice"][3 * i : 3 * i + 3].cpu().numpy()
-            else:
-                raise ValueError("No cell or lattice found in batch")
-            config = Configuration(
-                atomic_numbers=numbers,
-                positions=positions,
-                cell=cell,
-                pbc=(True, True, True),
-            )
-            dataset.append(
-                AtomicData.from_config(config, z_table=self.z_table, cutoff=self.r_max)
-            )
-
-        data_loader = torch_geometric.dataloader.DataLoader(
-            dataset=dataset,
-            batch_size=len(dataset),
-            shuffle=False,
-            drop_last=False,
-        )
-        data = next(iter(data_loader)).to(props["nxyz"].device)
-
-        return data
-
-    def add_engrad_to_output(self, output: dict) -> dict:
-        """Adds an energy gradient to the output dictionary, which is
-        computed from the force prediction
-
-        Args:
-            output (dict): dictionary of the output of a forward pass
-                through the model
-
-        Returns:
-            dict: updated output dictionary with "energy_grad" key and value
-        """
-        output["energy_grad"] = -output["forces"]
-        return output
-
-    @classmethod
-    def from_file(cls, path: str, **kwargs):
-        """Build a NFFMACEWrapper from a saved file."""
-        state = torch.load(path)
-        # extract initialization parameters from the state
-        return NFFMACEWrapper(mace_model=state, **kwargs)
+    return model
 
 
-class DirectNffScaleMACEWrapper(ScaleShiftMACE):
+class NffScaleMACE(ScaleShiftMACE):
     """Wrapper for the ScaleShiftMACE model that allows for direct
     forward passes with an AtomsBatch object in NFF
     """
 
     def __init__(
-        self, mace_model: Union[MACE, ScaleShiftMACE], default_dtype="", **kwargs
+        self,
+        mace_model: Union[MACE, ScaleShiftMACE],
+        default_dtype="",
+        training: bool = False,
+        **kwargs,
     ):
         atomic_inter_scale = mace_model.scale_shift.scale
         atomic_inter_shift = mace_model.scale_shift.shift
-        print(kwargs)
         super().__init__(atomic_inter_scale, atomic_inter_shift, **kwargs)
         self.mace_model = mace_model
         self.z_table = AtomicNumberTable(
             [int(z) for z in self.mace_model.atomic_numbers]
         )
+
+        # set the model to training mode if necessary because the
+        # MACE model is not set to training mode by default and takes
+        # the training argument in the forward pass
+        self.training = training
+
         model_dtype = get_model_dtype(self.mace_model)
         if default_dtype == "":
             print(
@@ -174,19 +120,45 @@ class DirectNffScaleMACEWrapper(ScaleShiftMACE):
                 self.mace_model = self.mace_model.float()
         torch_tools.set_default_dtype(default_dtype)
 
-    def forward(self, batch, **kwargs):
-        data = self.convert_atomsbatch_to_data(batch)
-        breakpoint()
-        output = super().forward(data, **kwargs)
+    def forward(
+        self,
+        batch: dict,
+        training: bool = False,  # retained to keep the same args as the original MACE model forward
+        compute_force: bool = True,
+        compute_virials: bool = False,
+        compute_stress: bool = False,
+        compute_displacement: bool = False,
+    ) -> dict:  # noqa: W0221
+        """Forward pass through the model
+
+        Args:
+            batch (dict): dictionary of Geom properties (energy, energy_grad, stress, etc.)
+            kwargs: additional keyword arguments for the super().forward() function from MACE
+
+        Returns:
+            dict: dict of output from the forward pass
+        """
+        data = self.convert_batch_to_data(batch)
+        output = super().forward(
+            data,
+            training=self.training,  # set the training mode to the value of the wrapper
+            compute_force=compute_force,
+            compute_virials=compute_virials,
+            compute_stress=compute_stress,
+            compute_displacement=compute_displacement,
+        )
         output = self.add_engrad_to_output(output)
         return output
 
-    def convert_atomsbatch_to_data(self, batch) -> torch_geometric.data.Data:
-        """Convert AtomsBatch object to a torch geometric data object
+    def convert_batch_to_data(self, batch: dict) -> torch_geometric.data.Data:
+        """Convert Batch object to a torch geometric data object
         that is used in the MACE model
 
         Args:
-            batch (AtomsBatch): an AtomsBatch object, similar to ASE Atoms
+            batch (dict): a batch object that contains the properties. The NFF
+                calculators automatically convert AtomsBatch objects to dictionaries
+                to be used in the forward pass. This function will also work with
+                Trainers that use the batches from Dataloaders directly.
 
         Raises:
             ValueError: raised if no cell or lattice is found in the AtomsBatch
@@ -194,15 +166,10 @@ class DirectNffScaleMACEWrapper(ScaleShiftMACE):
         Returns:
             torch_geometric.data.Data: torch geometric data object
         """
-        if isinstance(batch, AtomsBatch):
-            props = batch.get_batch()
-        elif isinstance(batch, dict):
+        if isinstance(batch, dict):
             props = batch
-        elif isinstance(batch, Batch):
-            return batch
         else:
-            print(type(batch))
-            raise ValueError("Batch must be an AtomsBatch, Batch, or dictionary")
+            raise ValueError("Batch must be a dictionary")
         cum_idx_list = [0] + torch.cumsum(props["num_atoms"], 0).tolist()
         dataset = []
 
@@ -256,7 +223,7 @@ class DirectNffScaleMACEWrapper(ScaleShiftMACE):
         return output
 
     @classmethod
-    def get_init_args_from_model(self, model: Union[ScaleShiftMACE, MACE]) -> dict:
+    def get_init_args_from_model(cls, model: Union[ScaleShiftMACE, MACE]) -> dict:
         """Get the initialization arguments from the model"""
         init_args = {
             "r_max": model.r_max.item(),
@@ -280,9 +247,23 @@ class DirectNffScaleMACEWrapper(ScaleShiftMACE):
         return init_args
 
     @classmethod
-    def from_file(cls, path: str, map_location: str = "cpu", **kwargs):
-        """Build a DirectNffScaleMACEWrapper from a saved file."""
+    def from_file(
+        cls, path: str, map_location: str = "cpu", default_dtype: str = ""
+    ) -> "NffScaleMACE":
+        """Build a NffScaleMACE from a saved file."""
         state = torch.load(path, map_location=map_location)
         # extract initialization parameters from the state
-        init_params = DirectNffScaleMACEWrapper.get_init_args_from_model(state)
-        return DirectNffScaleMACEWrapper(mace_model=state, **init_params)
+        init_params = NffScaleMACE.get_init_args_from_model(state)
+        return NffScaleMACE(
+            mace_model=state, default_dtype=default_dtype, **init_params
+        )
+
+    @classmethod
+    def load(
+        cls, model: str = "medium", map_location: str = "cpu", default_dtype: str = ""
+    ) -> "NffScaleMACE":
+        """Load a MACE model from a file or URL"""
+        mace_model = get_mace_mp_model(model)
+        return cls.from_file(
+            mace_model, map_location=map_location, default_dtype=default_dtype
+        )
