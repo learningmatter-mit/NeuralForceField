8da053a5ca65bfb05bf7329cb5dfc9265fe2d42d
alex <ajhoff29@mit.edu>
2024-04-15 18:11:59 -0400

    STY: ruff on graphs.py

--- nff/data/graphs.py
+++ nff/data/graphs.py
@@ -51,55 +51,57 @@ DISTANCETHRESHOLDICT_SYMBOL = {
     ("O", "F"): 1.50,
     ("O", "Si"): 1.85,
     ("O", "Br"): 1.70,
-    ("F", "Mg"): 1.35, }
+    ("F", "Mg"): 1.35,
+}
 
 DISTANCETHRESHOLDICT_Z = {
-    (1., 1.): 1.00,
-    (1., 3.): 1.30,
-    (1., 5.): 1.50,
-    (1., 6.): 1.30,
-    (1., 7.): 1.30,
-    (1., 8.): 1.30,
-    (1., 9.): 1.30,
-    (1., 11.): 1.65,
-    (1., 14.): 1.65,
-    (1., 12.): 1.40,
-    (1., 16.): 1.50,
-    (1., 17.): 1.60,
-    (1., 35.): 1.60,
-    (3., 6.): 0.0,
-    (3., 7.): 0.0,
-    (3., 8.): 0.0,
-    (3., 9.): 0.0,
-    (3., 12.): 0.0,
-    (5., 6.): 1.70,
-    (5., 7.): 1.70,
-    (5., 8.): 1.70,
-    (5., 9.): 1.70,
-    (5., 11.): 1.8,
-    (5., 12.): 1.8,
-    (5., 17.): 2.1,
-    (5., 35.): 2.1,
-    (6., 6.): 1.70,
-    (6., 8.): 1.70,
-    (6., 7.): 1.8,
-    (6., 9.): 1.65,
-    (6., 11.): 1.80,
-    (6., 12.): 1.70,
-    (6., 14.): 2.10,
-    (6., 16.): 2.20,
-    (7., 8.): 1.55,
-    (7., 11.): 1.70,
-    (7., 16.): 2.0,
-    (8., 11.): 1.70,
-    (8., 12.): 1.35,
-    (8., 16.): 2.00,
-    (8., 17.): 1.80,
-    (8., 8.): 1.70,
-    (8., 9.): 1.50,
-    (8., 14.): 1.85,
-    (8., 35.): 1.70,
-    (9., 12.): 1.35}
+    (1.0, 1.0): 1.00,
+    (1.0, 3.0): 1.30,
+    (1.0, 5.0): 1.50,
+    (1.0, 6.0): 1.30,
+    (1.0, 7.0): 1.30,
+    (1.0, 8.0): 1.30,
+    (1.0, 9.0): 1.30,
+    (1.0, 11.0): 1.65,
+    (1.0, 14.0): 1.65,
+    (1.0, 12.0): 1.40,
+    (1.0, 16.0): 1.50,
+    (1.0, 17.0): 1.60,
+    (1.0, 35.0): 1.60,
+    (3.0, 6.0): 0.0,
+    (3.0, 7.0): 0.0,
+    (3.0, 8.0): 0.0,
+    (3.0, 9.0): 0.0,
+    (3.0, 12.0): 0.0,
+    (5.0, 6.0): 1.70,
+    (5.0, 7.0): 1.70,
+    (5.0, 8.0): 1.70,
+    (5.0, 9.0): 1.70,
+    (5.0, 11.0): 1.8,
+    (5.0, 12.0): 1.8,
+    (5.0, 17.0): 2.1,
+    (5.0, 35.0): 2.1,
+    (6.0, 6.0): 1.70,
+    (6.0, 8.0): 1.70,
+    (6.0, 7.0): 1.8,
+    (6.0, 9.0): 1.65,
+    (6.0, 11.0): 1.80,
+    (6.0, 12.0): 1.70,
+    (6.0, 14.0): 2.10,
+    (6.0, 16.0): 2.20,
+    (7.0, 8.0): 1.55,
+    (7.0, 11.0): 1.70,
+    (7.0, 16.0): 2.0,
+    (8.0, 11.0): 1.70,
+    (8.0, 12.0): 1.35,
+    (8.0, 16.0): 2.00,
+    (8.0, 17.0): 1.80,
+    (8.0, 8.0): 1.70,
+    (8.0, 9.0): 1.50,
+    (8.0, 14.0): 1.85,
+    (8.0, 35.0): 1.70,
+    (9.0, 12.0): 1.35,
+}
 
 
 def get_neighbor_list(xyz, cutoff=5, undirected=True):
@@ -121,11 +123,10 @@ def get_neighbor_list(xyz, cutoff=5, undirected=True):
     n = xyz.size(0)
 
     # calculating distances
-    dist = (xyz.expand(n, n, 3) - xyz.expand(n, n, 3).transpose(0, 1)
-            ).pow(2).sum(dim=2).sqrt()
+    dist = (xyz.expand(n, n, 3) - xyz.expand(n, n, 3).transpose(0, 1)).pow(2).sum(dim=2).sqrt()
 
     # neighbor list
-    mask = (dist <= cutoff)
+    mask = dist <= cutoff
     mask[np.diag_indices(n)] = 0
     nbr_list = mask.nonzero(as_tuple=False)
 
@@ -158,8 +159,8 @@ def get_bond_idx(bonded_nbr_list, nbr_list):
         nbr_list (torch.LongTensor): pairs of atoms
             within a cutoff radius of each other.
     Returns:
-        bond_idx (torch.LongTensor): set of indices in the 
-            neighbor list that corresponds to the same 
+        bond_idx (torch.LongTensor): set of indices in the
+            neighbor list that corresponds to the same
             directed pair of atoms in the bond list.
     """
 
@@ -170,18 +171,17 @@ def get_bond_idx(bonded_nbr_list, nbr_list):
     nbr_dic = {to_tuple(pair): i for i, pair in enumerate(nbr_list)}
     # call the dictionary for each pair of atoms in the bonded neighbor
     # list to get `bond_idx`
-    bond_idx = torch.LongTensor([nbr_dic[to_tuple(pair)]
-                                 for pair in bonded_nbr_list])
+    bond_idx = torch.LongTensor([nbr_dic[to_tuple(pair)] for pair in bonded_nbr_list])
 
     return bond_idx
 
 
 def get_dist_mat(xyz, box_len, unwrap=True):
-    dis_mat = (xyz[:, None, :] - xyz[None, ...])
+    dis_mat = xyz[:, None, :] - xyz[None, ...]
 
     # build minimum image convention
-    mask_pos = dis_mat.ge(0.5*box_len).type(torch.FloatTensor)
-    mask_neg = dis_mat.lt(-0.5*box_len).type(torch.FloatTensor)
+    mask_pos = dis_mat.ge(0.5 * box_len).type(torch.FloatTensor)
+    mask_neg = dis_mat.lt(-0.5 * box_len).type(torch.FloatTensor)
 
     # modify distance
     if unwrap:
@@ -194,7 +194,7 @@ def get_dist_mat(xyz, box_len, unwrap=True):
     dis_sq = dis_mat.pow(2).sum(-1)
     # mask = (dis_sq <= cutoff ** 2) & (dis_sq != 0)
     # byte tensor of dim (B, N, N)
-    #A = mask.unsqueeze(3).type(torch.FloatTensor).to(self.device) #
+    # A = mask.unsqueeze(3).type(torch.FloatTensor).to(self.device) #
 
     # 1) PBC 2) # gradient of zero distance
     dis_sq = dis_sq.unsqueeze(-1)
@@ -209,15 +209,16 @@ def get_dist_mat(xyz, box_len, unwrap=True):
 
 
 def adjdistmat(atoms, threshold=DISTANCETHRESHOLDICT_Z, unwrap=True):
-    #dmat = (xyz[:, None, :] - xyz[None, ...]).pow(2).sum(-1).numpy()
+    # dmat = (xyz[:, None, :] - xyz[None, ...]).pow(2).sum(-1).numpy()
     xyz = torch.Tensor(atoms.get_positions(wrap=True))
     atomicnums = atoms.get_atomic_numbers().tolist()
     box_len = torch.Tensor(np.diag(atoms.get_cell()))
 
     dmat = get_dist_mat(xyz, box_len, unwrap=unwrap).numpy()
 
-    thresholdmat = np.array([[threshold.get(tuple(
-        sorted((i, j))), 2.0) for i in atomicnums] for j in atomicnums])
+    thresholdmat = np.array(
+        [[threshold.get(tuple(sorted((i, j))), 2.0) for i in atomicnums] for j in atomicnums]
+    )
     adjmat = (dmat < thresholdmat).astype(int)
 
     np.fill_diagonal(adjmat, 0)
@@ -229,16 +230,15 @@ def generate_mol_atoms(atomic_nums, xyz, cell):
 
 
 def generate_subgraphs(atomsobject, unwrap=True, get_edge=False):
-
     from nff.io.ase import AtomsBatch
 
     atoms = AtomsBatch(atomsobject)
-    z, adj, dmat,  threshold = adjdistmat(atoms, unwrap=unwrap)
+    z, adj, dmat, threshold = adjdistmat(atoms, unwrap=unwrap)
     box_len = torch.Tensor(np.diag(atoms.get_cell()))
     G = nx.from_numpy_matrix(adj)
 
     for i, item in enumerate(z):
-        G.nodes[i]['z'] = item
+        G.nodes[i]["z"] = item
 
     sub_graphs = nx.connected_component_subgraphs(G)
 
@@ -258,8 +258,7 @@ def generate_subgraphs(atomsobject, unwrap=True, get_edge=False):
 def get_single_molecule(atomsobject, mol_idx, single_mol_id):
     z = atomsobject.get_atomic_numbers()[mol_idx[single_mol_id]]
     pos = atomsobject.get_positions()[mol_idx[single_mol_id]]
-    return Atoms(numbers=z, positions=pos,
-                 cell=atomsobject.cell, pbc=True)
+    return Atoms(numbers=z, positions=pos, cell=atomsobject.cell, pbc=True)
 
 
 def reconstruct_atoms(atomsobject, mol_idx):
@@ -269,7 +268,7 @@ def reconstruct_atoms(atomsobject, mol_idx):
     print(box_len)
     for idx in mol_idx:
         mol_xyz = sys_xyz[idx]
-        center = mol_xyz.shape[0]//2
+        center = mol_xyz.shape[0] // 2
         intra_dmat = (mol_xyz[None, ...] - mol_xyz[:, None, ...])[center]
         sub = (intra_dmat > 0.5 * box_len).to(torch.float) * box_len
         add = (intra_dmat <= -0.5 * box_len).to(torch.float) * box_len
@@ -286,7 +285,7 @@ def list2adj(bond_list, size=None):
     if size is None:
         size = max(set([n for e in E for n in e])) + 1
     # make an empty adjacency list
-    adjacency = [[0]*size for _ in range(size)]
+    adjacency = [[0] * size for _ in range(size)]
     # populate the list for each edge
     for sink, source in E:
         adjacency[sink][source] = 1
@@ -303,7 +302,7 @@ def make_directed(nbr_list):
         new_nbrs (torch.LongTensor): directed neighbor
             list
         directed (bool): whether the old one was directed
-            or not  
+            or not
     """
 
     gtr_ij = (nbr_list[:, 0] > nbr_list[:, 1]).any().item()
@@ -362,8 +361,7 @@ def get_angle_list(nbr_lists):
             nbr_1 = nbr[1].item()
             nbr_1_nbrs = torch.LongTensor(nbr_dic[nbr_1]).reshape(-1, 1)
             nbr_repeat = nbr.repeat(len(nbr_1_nbrs), 1)
-            these_angles += [torch.cat([nbr_repeat,
-                                        nbr_1_nbrs], dim=-1)]
+            these_angles += [torch.cat([nbr_repeat, nbr_1_nbrs], dim=-1)]
         these_angles = torch.cat(these_angles)
 
         new_nbrs.append(nbr_list)
@@ -373,16 +371,12 @@ def get_angle_list(nbr_lists):
     # take out angles of the form [i, j, i], which aren't really angles
     angle_tens = torch.cat(angles)
     mask = angle_tens[:, 0] != angle_tens[:, 2]
-    angles = list(torch.split(angle_tens[mask],
-                              num))
+    angles = list(torch.split(angle_tens[mask], num))
 
     return angles, new_nbrs
 
 
-def m_idx_of_angles(angle_list,
-                    nbr_list,
-                    angle_start,
-                    angle_end):
+def m_idx_of_angles(angle_list, nbr_list, angle_start, angle_end):
     """
     Get the array index of elements of an angle list.
     Args:
@@ -444,7 +438,7 @@ def m_idx_of_angles(angle_list,
     reshaped_angle = angle_list[:, angle_end].reshape(-1, 1)
 
     # the full mask is the product of both
-    mask *= (repeated_nbr == reshaped_angle)
+    mask *= repeated_nbr == reshaped_angle
 
     # get the indices where everything is true
     idx = mask.nonzero(as_tuple=False)[:, 1]
@@ -479,15 +473,13 @@ def add_ji_kj(angle_lists, nbr_lists):
     kj_idx_list = []
     for i, nbr_list in tqdm_enum(nbr_lists):
         angle_list = angle_lists[i]
-        ji_idx = m_idx_of_angles(angle_list=angle_list,
-                                 nbr_list=nbr_list,
-                                 angle_start=1,
-                                 angle_end=0)
+        ji_idx = m_idx_of_angles(
+            angle_list=angle_list, nbr_list=nbr_list, angle_start=1, angle_end=0
+        )
 
-        kj_idx = m_idx_of_angles(angle_list=angle_list,
-                                 nbr_list=nbr_list,
-                                 angle_start=2,
-                                 angle_end=1)
+        kj_idx = m_idx_of_angles(
+            angle_list=angle_list, nbr_list=nbr_list, angle_start=2, angle_end=1
+        )
         ji_idx_list.append(ji_idx)
         kj_idx_list.append(kj_idx)
 
@@ -495,49 +487,48 @@ def add_ji_kj(angle_lists, nbr_lists):
 
 
 def make_dset_directed(dset):
-    """
-    Make everything in the dataset correspond to a directed 
+    """Make everything in the dataset correspond to a directed
     neighbor list.
+
     Args:
         dset (nff.data.Dataset): nff dataset
+
     Returns:
         None
     """
-
     # make the neighbor list directed
     for i, batch in enumerate(dset):
-        nbr_list, nbr_was_directed = make_directed(batch['nbr_list'])
-        dset.props['nbr_list'][i] = nbr_list
+        nbr_list, nbr_was_directed = make_directed(batch["nbr_list"])
+        dset.props["nbr_list"][i] = nbr_list
 
         # fix bond_idx
         bond_idx = batch.get("bond_idx")
-        has_bond_idx = (bond_idx is not None)
+        has_bond_idx = bond_idx is not None
         if (not nbr_was_directed) and has_bond_idx:
             nbr_dim = nbr_list.shape[0]
-            bond_idx = torch.cat([bond_idx,
-                                  bond_idx + nbr_dim // 2])
-            dset.props['bond_idx'][i] = bond_idx
+            bond_idx = torch.cat([bond_idx, bond_idx + nbr_dim // 2])
+            dset.props["bond_idx"][i] = bond_idx
 
         # make the bonded nbr list directed
-        bond_nbrs = batch.get('bonded_nbr_list')
-        has_bonds = (bond_nbrs is not None)
+        bond_nbrs = batch.get("bonded_nbr_list")
+        has_bonds = bond_nbrs is not None
         if has_bonds:
             bond_nbrs, bonds_were_directed = make_directed(bond_nbrs)
-            dset.props['bonded_nbr_list'][i] = bond_nbrs
+            dset.props["bonded_nbr_list"][i] = bond_nbrs
 
         # fix the corresponding bond features
-        bond_feats = batch.get('bond_features')
-        has_bond_feats = (bond_feats is not None)
+        bond_feats = batch.get("bond_features")
+        has_bond_feats = bond_feats is not None
         if (has_bonds and has_bond_feats) and (not bonds_were_directed):
             bond_feats = torch.cat([bond_feats] * 2, dim=0)
-            dset.props['bond_features'][i] = bond_feats
+            dset.props["bond_features"][i] = bond_feats
 
 
 def batch_angle_idx(nbrs):
     """
     Given a neighbor list, find the sets of indices in the neighbor list
-    corresponding to the kj and ji indices. Usually you can only do this 
-    for one conformer without running out of memory -- to do it for multiple 
+    corresponding to the kj and ji indices. Usually you can only do this
+    for one conformer without running out of memory -- to do it for multiple
     conformers, use `full_angle_idx` below.
     Args:
         nbrs (torch.LongTensor): neighbor list
@@ -549,8 +540,7 @@ def batch_angle_idx(nbrs):
     """
 
     all_idx = torch.stack([torch.arange(len(nbrs))] * len(nbrs)).long()
-    mask = ((nbrs[:, 1] == nbrs[:, 0, None])
-            * (nbrs[:, 0] != nbrs[:, 1, None]))
+    mask = (nbrs[:, 1] == nbrs[:, 0, None]) * (nbrs[:, 0] != nbrs[:, 1, None])
     ji_idx = all_idx[mask]
     kj_idx = mask.nonzero(as_tuple=False)[:, 0]
 
@@ -569,9 +559,9 @@ def full_angle_idx(batch):
             value of n.
     """
 
-    nbr_list = batch['nbr_list']
-    num_atoms = batch['num_atoms']
-    mol_size = batch.get('mol_size', num_atoms)
+    nbr_list = batch["nbr_list"]
+    num_atoms = batch["num_atoms"]
+    mol_size = batch.get("mol_size", num_atoms)
     num_confs = num_atoms // mol_size
 
     all_ji_idx = []
@@ -582,16 +572,14 @@ def full_angle_idx(batch):
         min_idx = (i) * mol_size
 
         # get only the indices for this conformer
-        conf_mask = ((nbr_list[:, 0] < max_idx) *
-                     (nbr_list[:, 0] >= min_idx))
+        conf_mask = (nbr_list[:, 0] < max_idx) * (nbr_list[:, 0] >= min_idx)
         nbrs = nbr_list[conf_mask]
         # map from indices of these sub-neighbors
         # to indices in full neighbor list
         ji_idx, kj_idx = batch_angle_idx(nbrs)
 
         # map to these indices
-        map_indices = (conf_mask.nonzero(as_tuple=False)
-                       .reshape(-1))
+        map_indices = conf_mask.nonzero(as_tuple=False).reshape(-1)
         all_ji_idx.append(map_indices[ji_idx])
         all_kj_idx.append(map_indices[kj_idx])
 
@@ -617,15 +605,17 @@ def kj_ji_to_dset(dataset, track):
     if track:
         iter_func = tqdm
     else:
-        def iter_func(x): return x
+
+        def iter_func(x):
+            return x
 
     for batch in iter_func(dataset):
         ji_idx, kj_idx = full_angle_idx(batch)
         all_ji_idx.append(ji_idx)
         all_kj_idx.append(kj_idx)
 
-    dataset.props['ji_idx'] = all_ji_idx
-    dataset.props['kj_idx'] = all_kj_idx
+    dataset.props["ji_idx"] = all_ji_idx
+    dataset.props["kj_idx"] = all_kj_idx
 
     return dataset
 
@@ -644,7 +634,9 @@ def add_bond_idx(dataset, track):
     if track:
         iter_func = tqdm
     else:
-        def iter_func(x): return x
+
+        def iter_func(x):
+            return x
 
     dataset.props["bond_idx"] = []
 
