05452f910afe36aaf2aacc1e5f0861e5947db6bd
steinmig <miguel.steiner@phys.chem.ethz.ch>
2024-12-17 13:20:08 -0500

    remove print

--- nff/tests/conftest.py
+++ nff/tests/conftest.py
@@ -7,7 +7,6 @@ torch.set_num_threads(int(os.getenv("OMP_NUM_THREADS", 1)))
 
 
 def pytest_addoption(parser):
-    print("add option")
     parser.addoption("--device", action="store", default="cpu", help="Whether to use the CPU or GPU for the tests")
 
 
